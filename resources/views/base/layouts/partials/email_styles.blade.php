45bd901232a7afc13bfac7de410cad093e383e11
Magali <magali@fanbridge.com>
2016-04-04 15:59:58 -0300

    bring branch ST-857-lderossi_v1.3.0 #noreview

--- resources/views/base/layouts/partials/email_styles.blade.php
+++ resources/views/base/layouts/partials/email_styles.blade.php
@@ -1,10 +1,10 @@
 <style type="text/css">
-	a {
+    a {
         text-decoration: none
     }
 
-	/* Client-specific Styles */
-	body.email-body {
+    /* Client-specific Styles */
+    body.email-body {
         width:100% !important;
         -webkit-text-size-adjust: 100%;
         margin: 0;
@@ -13,74 +13,74 @@
     }
 
     /* Force Outlook to provide a "view in browser" menu link. */
-	#outlook a {
+    #outlook a {
         padding: 0;
         text-decoration: none !important;
     }
 
-	#backgroundTable {
+    #backgroundTable {
         margin:0;
         padding:0;
         width:100% !important;
     }
 
     /* Force Hotmail to display emails at full width*/
-	.ExternalClass {
+    .ExternalClass {
         width:100%;
     }
-	.ExternalClass * {
+    .ExternalClass * {
         line-height: 100%;
     }
     /*Force Hotmail to display normal line spacing.  More on that: http://www.emailonacid.com/forum/viewthread/43/ */
-	.ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {
+    .ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {
         line-height: 100%;
     }
 
-	/*css generic*/
-	a,a:hover,a:link,a:visited {
+    /*css generic*/
+    a,a:hover,a:link,a:visited {
         text-decoration:none !important;
         outline: none;
     }
 
-	hr {
+    hr {
         display: none !important;
     }
 
-	@media screen and (max-width: 480px) {
+    @media screen and (max-width: 480px) {
 
         img {
             max-width: 320px !important;
         }
 
-		table.wrapper-table {
+        table.wrapper-table {
             width: 100% !important;
         }
-		td.element-block-center {
+        td.element-block-center {
             display: block !important;
             width: 100% !important;
             text-align: center !important;
             padding: 0 !important;
         }
-		td.text-align-center {
+        td.text-align-center {
             text-align: center !important;
         }
-		td.mobile-margin {
+        td.mobile-margin {
             padding: 0 10px !important;
         }
-		tr.show-device {
+        tr.show-device {
             display: block !important;
         }
-		*.hidden-device {
+        *.hidden-device {
             display: none !important;
         }
-		div.show-img-device {
-			display: table !important;
-			width: 100% !important;
-			float: none;
-			overflow: visible !important;
-			height: auto !important;
-		}
-		.display-mobile {
+        div.show-img-device {
+            display: table !important;
+            width: 100% !important;
+            float: none;
+            overflow: visible !important;
+            height: auto !important;
+        }
+        .display-mobile {
             display: block !important;
             width: 100% !important;
             overflow: visible !important;
@@ -94,22 +94,22 @@
         .full-width {
             width: 100% !important;
         }
-	}
+    }
 </style>
 
 <!--[if mso]>
-	<style>
-		table[module-table-wrapper] {
+    <style>
+        table[module-table-wrapper] {
             width: 560px !important;
         }
-		td, span, p, a, h5, h6, div {
+        td, span, p, a, h5, h6, div {
             font-family: Arial, Helvetica, sans-serif !important;
         }
-	</style>
+    </style>
 <![endif]-->
 
 <!--[if IEMobile]>
-	<style type="text/css">
-		td, span, p, a, h5, h6, div{font-family: Arial, Helvetica, sans-serif !important;}
-	</style>
+    <style type="text/css">
+        td, span, p, a, h5, h6, div{font-family: Arial, Helvetica, sans-serif !important;}
+    </style>
 <![endif]-->
