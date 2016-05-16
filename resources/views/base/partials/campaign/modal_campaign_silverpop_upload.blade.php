e185ff9b98997e1cf234477b97ee401db74640fa
jony <jony@fanbridge.com>
2016-05-16 15:59:51 -0300

    updated clases to fix status message

--- resources/views/base/partials/campaign/modal_campaign_silverpop_upload.blade.php
+++ resources/views/base/partials/campaign/modal_campaign_silverpop_upload.blade.php
@@ -6,10 +6,10 @@
                     <h4>Upload to {{ $app_config['api']['silverpop']['title'] }}</h4>
                 </div>
                 <div class="modal-body">
-                    <div class="response-message-success alert alert-success" style="display:none;">
+                    <div class="response-message response-message-success alert alert-success" style="display:none;">
                         Email successfully uploaded to {{ $app_config['api']['silverpop']['title'] }}. <a href="{{ url('/') }}" class="allow-exit">View dashboard</a>.
                     </div>
-                    <div class="response-message-error alert alert-danger" style="display:none;">Something went wrong. Try again later.</div>
+                    <div class="response-message response-message-error alert alert-danger" style="display:none;">Something went wrong. Try again later.</div>
                     <form name="upload-api-form" class="upload-api-form">
                         <input type="hidden" name="campaign_id" value="" class="campaign_id" />
                         <input type="hidden" name="api_driver" value="{{ $app_config['api']['silverpop']['class'] }}" class="api_driver" />
