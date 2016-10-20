faa5ac4a069d922090e12f19eef91a174baed2ba
Alejandro <alejandro@fanbridge.com>
2016-10-20 17:07:55 -0300

    script updates

--- resources/views/base/layouts/partials/debug.blade.php
+++ resources/views/base/layouts/partials/debug.blade.php
@@ -1,11 +1,13 @@
-<script type="text/javascript" src="{{ asset('js/trace.js') }}"></script>
+{{-- this url will be change later --}}
+<script type="text/javascript" src="{{ asset('js/debug.js') }}"></script>
 <script type="text/javascript">
     (function(win) {
         stensul_stacktrace.init(win, {
             'app': '{{ strtolower(config('app.name')) }}',
             'url': '{{ url() }}',
             'user_id': '{{ auth()->check() ? auth()->user()->id : 'no-logged-user' }}',
-            'environment': '{{ app('env') }}'
+            'environment': '{{ app('env') }}',
+            'version': '{{ config('app.version') }}'
         }, {
             'debug': {{ config('app.debug') }}
         })
