4c5389a154e5fad4f57148ae3c8783cb6ddca921
Juan <juan.ferrarib@gmail.com>
2016-10-31 16:37:54 -0300

    fixed queue failing event

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,6 +5,7 @@ namespace Stensul\Providers;
 use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Queue\Events\JobFailed;
+use Illuminate\Support\Facades\Queue;
 
 class EventServiceProvider extends ServiceProvider
 {
