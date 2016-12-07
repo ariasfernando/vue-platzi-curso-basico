70e9259ae84d992a26e5a24fd4100ef23c08a917
Juan <juan.ferrarib@gmail.com>
2016-12-07 12:56:10 -0300

    small fix

--- resources/views/base/admin/partials/library_draw_row.blade.php
+++ resources/views/base/admin/partials/library_draw_row.blade.php
@@ -2,7 +2,7 @@
 	<tr data-library="{{ $library->id }}">
 		<td title="{{ $library->name }}">{{ $library->name }}</td>
 		<td title="{{ $library->description }}">{{ $library->description }}</td>
-		<td title="{{ join(", ",$library->modules) }}">{{ join(",",$library->modules) }}</td>
+		<td title="{{ join(", ",$library->modules) }}">{{ join(", ",$library->modules) }}</td>
 		<td>{{ date("m/d/Y",strtotime($library->created_at)) }}<br>{{ date("h:i a T",strtotime($library->created_at)) }}</td>
 		<td class="text-right actions icons">
 			<a href="#" class="edit" title="Edit"><i class="glyphicon glyphicon-pencil"></i></a>
