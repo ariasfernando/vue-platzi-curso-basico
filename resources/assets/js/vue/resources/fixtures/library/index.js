cf4638212129e69a935e155bcdd36e89e41b7aaf
Emiliano Mongelo <emiliano@stensul.com>
2017-06-08 16:16:43 -0300

    Fixtures

---
+++ resources/assets/js/vue/resources/fixtures/library/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  getLibrary: require('./getLibrary'),
+  newLibrary: require('./newLibrary'),
+  saveLibrary: require('./saveLibrary'),
+  createLibrary: require('./createLibrary'),
+  deleteLibrary: require('./deleteLibrary'),
+  fetchLibraries: require('./fetchLibrary')
+};
---
+++ resources/assets/js/vue/resources/fixtures/module/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  getModule: require('./getModule'),
+  getAllModules: require('./getAllModule'),
+  saveModule: require('./saveModule'),
+  createModule: require('./createModule'),
+  deleteModule: require('./deleteModule')
+};
