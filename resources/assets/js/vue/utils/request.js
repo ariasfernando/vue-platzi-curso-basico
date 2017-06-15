31e401b65e9fd27bb88737c4c52529161db9a1d6
Emiliano Mongelo <emiliano@stensul.com>
2017-06-15 18:33:10 -0300

    [SV2] Change request lib

--- resources/assets/js/vue/utils/request.js
+++ resources/assets/js/vue/utils/request.js
@@ -1,6 +1,9 @@
 import Q from 'q'
-import request from 'request'
 import _ from 'underscore'
+import Vue from 'vue/dist/vue'
+
+import VueResource from 'vue-resource/dist/vue-resource'
+Vue.use(VueResource);
 
 function requestResponse(method, params, options) {
   let deferred = Q.defer();
@@ -12,25 +15,22 @@ function requestResponse(method, params, options) {
   options.formData = params.formData || undefined;
   options.json = params.json || undefined;
 
-  let xCsrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content') || '';
-  options.headers = {
-    'X-CSRF-TOKEN': xCsrfToken
-  };
+  Vue.http[method](options.url, options.json)
+    .then(function (response) {
+      let statusCode = (response && response.status) ? response.status : 500;
 
-  request[method](options, function (error, response, body) {
-    // nnn
-    response = response || undefined;
-    let statusCode = (response && response.statusCode) ? response.statusCode : 500;
-
-    if (statusCode !== 200) {
-      deferred.reject(error);
-    } else {
-      if ( _.isString(response.body) ) {
-        response.body = JSON.parse(response.body);
+      if (statusCode !== 200) {
+        deferred.reject(error);
+      } else {
+        if ( _.isString(response.body) ) {
+          response.body = JSON.parse(response.body);
+        }
+        deferred.resolve(response);
       }
-      deferred.resolve(response);
-    }
-  });
+    })
+    .catch((error) => {
+      deferred.reject(error);
+    });
 
   return deferred.promise;
 }
