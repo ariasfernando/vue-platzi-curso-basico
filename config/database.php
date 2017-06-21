9e5f434a5fa8bc52d51cc6bd63ae812de17e8f04
Juan <juan@stensul.com>
2017-06-21 14:00:42 -0300

    #SV2-3 ci unit testin

--- config/database.php
+++ config/database.php
@@ -58,7 +58,7 @@ $default = [
         ),
         'mongodb_testing' => array(
             'driver'   => 'mongodb',
-            'host'     => env('DB_HOST', 'localhost'),
+            'host'     => env('DB_HOST', 'documentstore'),
             'port'     => env('DB_PORT', 27017),
             'username' => env('DB_USERNAME', ''),
             'password' => env('DB_PASSWORD', ''),
