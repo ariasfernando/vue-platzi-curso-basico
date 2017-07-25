2f125f58161224b58905b1f0d29b5644df30662d
Maxi Di Carlo <maxi@fanbridge.com>
2017-07-25 16:18:44 -0300

    [DES-32] admin design

--- resources/assets/js/library/admin/sidebar.js
+++ resources/assets/js/library/admin/sidebar.js
@@ -4,4 +4,10 @@ $trigger.click(function(e){
 	e.preventDefault();
 	$(this).siblings('ul.beta-menu-dropdown').slideToggle();
 	$(this).siblings('.glyphicon-menu-down').toggleClass('glyphicon-menu-up');
+
 });
+
+var $url = window.location.href;
+$('.beta-menu-dropdown a[href="'+$url+'"]').parent().parent().addClass('st-menu-expanded');
+$('.beta-menu-dropdown a[href="'+$url+'"]').addClass('st-active');
+$('.beta-menu-dropdown a[href="'+$url+'"]').parent().parent().siblings('.glyphicon-menu-down').toggleClass('glyphicon-menu-up');
