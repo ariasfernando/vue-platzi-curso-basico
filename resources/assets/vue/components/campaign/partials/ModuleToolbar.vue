38d7a1c709f18abf70645b3b2811ea2bf3b05daa
Emiliano Mongelo <emiliano@stensul.com>
2017-07-31 17:31:18 -0300

    [SV2] Add toolbar

---
+++ resources/assets/vue/components/campaign/partials/ModuleToolbar.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="module-toolbar">
+    <div class="icon-move"><i class="fa fa-arrows"></i></div>
+    <div class="icon-clone" @click="clone"><i class="fa fa-clone"></i></div>
+    <div class="icon-remove" @click="remove"><i class="fa fa-trash-o"></i></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ModuleToolbar',
+    props: ['moduleId'],
+    methods: {
+      clone(){
+        this.$store.commit("campaign/cloneModule", this.moduleId);
+      },
+      remove() {
+        this.$store.commit("campaign/removeModule", this.moduleId);
+      },
+    },
+  };
+</script>
+
+<style lang="less">
+  @icon-option: #69dac8;
+  @focus: #69dac8;
+  @focus-light: lighten(@focus, 30%);
+  @hover: @focus-light;
+
+  .st-module-wrapper {
+    &:hover {
+      border: 1px solid @icon-option;
+      background-color: @hover;
+      .icon-move, .icon-remove, .icon-clone {
+        display: block;
+      }
+    }
+  }
+
+  .st-position-relative {
+    position: relative;
+  }
+
+  .module-toolbar {
+    div {
+      cursor: pointer;
+      display: none;
+      position: absolute;
+      top: 20%;
+      text-align: center;
+      color: #fff;
+      z-index: 5;
+      height: 30px;
+      width: 30px;
+      border-radius: 100%;
+      line-height: 30px;
+      background-color: @icon-option;
+      opacity: 1;
+    }
+    .icon-clone {
+      right: -20px;
+    }
+    .icon-move {
+      cursor: move;
+      cursor: -webkit-grabbing;
+      right: -45px;
+    }
+    .icon-remove {
+      right: -70px;
+    }
+  }
+</style>
