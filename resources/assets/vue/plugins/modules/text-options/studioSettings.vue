bd28fb10317fb11ae70954c61f8b82ea46ce5e27
Emiliano Mongelo <emiliano@stensul.com>
2017-08-17 17:35:07 -0300

    [SV2] Missing files

---
+++ resources/assets/vue/plugins/modules/text-options/studioSettings.vue
@@ -0,0 +1,74 @@
+<template>
+  <div :class="'plugin-' + plugin.name">
+    <form class="form-horizontal">
+      <div class="form-group">
+        <label class="col-sm-7 control-label"><b>{{ plugin.title }}</b></label>
+        <div class="col-sm-5">
+          <span>
+            <toggle-button :value="enabled" color="#78DCD6" :sync="true" :labels="true" @change="toggle"></toggle-button>
+          </span>
+        </div>
+      </div>
+      <div class="form-group" v-for="(option, name) in options">
+        <label class="col-sm-7 control-label" :data-name="name"><b>{{ option.label }}</b></label>
+        <div class="col-sm-5">
+          <span>
+            <toggle-button :disabled="!enabled" :value="option.value" :name="name" color="#78DCD6" :sync="true" :labels="true" @change="toggleOption"></toggle-button>
+          </span>
+        </div>
+      </div>
+    </form>
+
+  </div>
+</template>
+
+<script>
+
+  import _ from 'lodash';
+
+  export default {
+    props: ['name', 'plugin'],
+    computed: {
+      currentComponent() {
+        return this.$store.getters["module/currentComponent"];
+      },
+    },
+    data() {
+      return {
+        options: this.plugin.data.options,
+        enabled: this.plugin.enabled,
+      }
+    },
+    methods: {
+      toggle(e) {
+        const payload = {
+          plugin: this.name,
+          columnId: this.currentComponent.columnId,
+          componentId: this.currentComponent.componentId,
+          enabled: e.value,
+        };
+
+        this.$store.commit('module/togglePlugin', payload);
+      },
+      toggleOption(e) {
+        const parentElement = e.srcEvent.target.parentElement;
+        const option = parentElement.attributes.getNamedItem('name').value;
+        const options = {};
+        options[option] = {
+          value: e.value
+        };
+
+        const payload = {
+          plugin: this.name,
+          columnId: this.currentComponent.columnId,
+          componentId: this.currentComponent.componentId,
+          data: {
+            options,
+          },
+        };
+
+        this.$store.commit('module/savePlugin', payload);
+      }
+    }
+  }
+</script>
