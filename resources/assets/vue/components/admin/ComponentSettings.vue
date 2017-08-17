41060eaae30e1a5f49427196518f90e749503bdb
Andres Lopez <andres@fanbridge.com>
2017-08-17 11:47:35 -0300

    add function collaps in column right

--- resources/assets/vue/components/admin/ComponentSettings.vue
+++ resources/assets/vue/components/admin/ComponentSettings.vue
@@ -1,35 +1,52 @@
 <template>
   <div class="component-settings" v-if="ready">
-    <h4>STYLES</h4>
-    <div class="default-settings">
-      <form class="form-horizontal">
-        <div class="form-group" :class="'field-' + setting.name" v-for="(setting, key) in component.settings">
-          <label class="col-sm-7 pull-left control-label" :for="setting.name">{{ setting.label }}</label>
-          <div class="col-sm-5 pull-right">
-            <input v-if="setting.type === 'text'" v-model="setting.value" v-validate="'required'" :class="{'input': true, 'is-danger': errors.has(setting.name) }"
-                   :name="setting.name" type="text" :placeholder="setting.label" @change="saveComponent">
+    
+    <!-- START: Style -->
+    <b-btn block v-b-toggle.style class="module-settings-item-right">
+      <p class="pull-left">STYLES</p>
+      <i class="glyphicon glyphicon-menu-down menu-dropdown pull-right"></i>
+    </b-btn>
 
-            <span v-if="setting.type === 'select'">
-              <b-form-select v-model="selected" :options="setting.value" class="mb-3" @change="saveComponent">
-              </b-form-select>
-            </span>
+    <b-collapse id="style" accordion="module-settings-accordion-right">
+      <b-card class="default-settings">
+        <form class="form-horizontal">
+          <div class="form-group" :class="'field-' + setting.name" v-for="(setting, key) in component.settings">
+            <label class="col-sm-7 pull-left control-label" :for="setting.name">{{ setting.label }}</label>
+            <div class="col-sm-5 pull-right">
+              <input v-if="setting.type === 'text'" v-model="setting.value" v-validate="'required'" :class="{'input': true, 'is-danger': errors.has(setting.name) }"
+                     :name="setting.name" type="text" :placeholder="setting.label" @change="saveComponent">
 
-            <span v-if="setting.type === 'switch'">
-              <toggle-button :value="setting.value" color="#78DCD6" :sync="true" :labels="true" @change="changeSetting(key, setting)"></toggle-button>
-            </span>
+              <span v-if="setting.type === 'select'">
+                <b-form-select v-model="selected" :options="setting.value" class="mb-3" @change="saveComponent">
+                </b-form-select>
+              </span>
 
-            <span v-show="errors.has(setting.name)" class="help is-danger">{{ errors.first(setting.name) }}</span>
+              <span v-if="setting.type === 'switch'">
+                <toggle-button :value="setting.value" color="#78DCD6" :sync="true" :labels="true" @change="changeSetting(key, setting)"></toggle-button>
+              </span>
+
+              <span v-show="errors.has(setting.name)" class="help is-danger">{{ errors.first(setting.name) }}</span>
+            </div>
           </div>
-        </div>
-      </form>
-    </div>
+        </form>
+      </b-card>
+    </b-collapse>
+    <!-- END: Style -->
 
-    <h4>FUNCTIONALITIES</h4>
-    <div class="plugins">
-      <div v-for="(plugin, key) in component.plugins" class="plugin-wrapper" :class="'plugin-' + plugin.name">
+    <!-- START: Funcionalities -->
+    <b-btn block v-b-toggle.funcionalities class="module-settings-item-right">
+      <p class="pull-left">FUNCTIONALITIES</p>
+      <i class="glyphicon glyphicon-menu-down menu-dropdown pull-right"></i>
+    </b-btn>
+
+    <b-collapse id="funcionalities" accordion="module-settings-accordion-right">
+      <b-card class="plugins">
+        <div v-for="(plugin, key) in component.plugins" class="plugin-wrapper" :class="'plugin-' + plugin.name">
         <component :is="'studio-' + plugin.name" :name="key" :plugin="plugin"></component>
       </div>
-    </div>
+      </b-card>
+    </b-collapse>
+    <!-- END: Funcionalities -->
 
   </div>
 </template>
@@ -113,5 +130,33 @@
   .plugin-wrapper {
     border-bottom: 1px solid #ccc;
     margin-bottom: 15px;
+  }
+
+  button.module-settings-item-right{
+    line-height: 13px;
+    box-shadow: none;
+    border-bottom: 1px solid #F0F0F0;
+    border-top: 0;
+    border-left: 0;
+    border-right: 0;
+    padding: 15px 10px 13px 10px; 
+
+    &:hover, &:visited,
+    &:focus, &:active,
+    &:active:focus{
+      color: #666666;
+      outline: none;
+      box-shadow: none;
+    }
+    p{
+      font-size: 13px;
+      margin: 0;
+      padding: 0;
+      font-weight: 300;
+    }
+    i{
+      color:#CCCCCC;
+      line-height: 12px;
+    }
   }
 </style>
