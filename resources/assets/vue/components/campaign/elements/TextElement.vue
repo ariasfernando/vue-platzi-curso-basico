36db615c59c0fbed4af24bbc594a22a9c58dea47
Emiliano Mongelo <emiliano@stensul.com>
2017-08-17 19:28:06 -0300

    [SV2] Text editor plugin

--- resources/assets/vue/components/campaign/elements/TextElement.vue
+++ resources/assets/vue/components/campaign/elements/TextElement.vue
@@ -2,15 +2,13 @@
   <!-- TEXT ELEMENT -->
   <tr data-type="text-element" @click.prevent="setComponent">
     <td width="100%" class="st-text-style st-position-relative" align="center" :style="component.style">
-      <p v-if="this.fixed">{{ component.text }}</p>
-      <tiny-mce v-else :id="editorId" :toolbar="toolbar" :value="component.text" data-key="text"></tiny-mce>
+      <div class="st-edit-text" :id="editorId" v-html="component.text"></div>
     </td>
   </tr>
   <!-- TEXT ELEMENT ENDS -->
 </template>
 
 <script>
-  import TinyMCE from './TinyMce.vue';
   import _ from 'lodash';
 
   export default {
@@ -21,33 +19,14 @@
       'component-id',
       'component'
     ],
-    components: {
-      'tiny-mce': TinyMCE
-    },
     data(){
       return {
         editorId: ['editor', this.moduleId, this.columnId, this.componentId].join('-'),
-        toolbar: '',
+        toolbar: ' ',
         fixed: false,
       }
     },
-    created () {
-      this.setupModule();
-    },
     methods: {
-      setupModule() {
-        if (this.component.plugins && this.component.plugins.richEditor) {
-          _.each(this.component.plugins.richEditor.fields, (option) => {
-            if (option.value === true) {
-              this.toolbar += ' ' + option.name;
-            }
-
-            if (option.name === 'fixed') {
-              this.fixed = option.value;
-            }
-          });
-        }
-      },
       setComponent(e) {
         if (!$(e.target).hasClass("st-remove")){
           this.$store.commit("campaign/setCurrentComponent", {
--- resources/assets/vue/plugins/modules/text-options/campaignSettings.vue
+++ resources/assets/vue/plugins/modules/text-options/campaignSettings.vue
@@ -20,10 +20,44 @@
       const options = _.filter(this.plugin.data.options, 'value');
       let toolbar = [];
 
-      _.each(options, (option) => {
-        toolbar.push(option.key)
-      });
-      this.$store.commit('campaign/setEditorOptions', toolbar.join(' '));
+      if (toolbar.length) {
+        _.each(options, (option) => {
+          toolbar.push(option.key)
+        });
+        toolbar = toolbar.join(' ');
+      } else {
+        toolbar = ' ';
+      }
+
+      const editorId = ['editor', this.currentComponent.moduleId, this.currentComponent.columnId, this.currentComponent.componentId].join('-');
+
+      let settings = {
+        selector: `#${editorId}`,
+        document_base_url: Application.globals.cdnHost + "/js/tinymce/",
+        skin: 'lightgray',
+        skin_url: Application.globals.cdnHost + '/css/tinymce/lightgray',
+        inline: true,
+        menubar: false,
+        toolbar: toolbar,
+        init_instance_callback: (editor) => {
+
+          editor.on('Blur', (e) => {
+            const parts = this.id.split('-');
+
+            this.$store.dispatch('campaign/updateElement', {
+              moduleId: parts[1],
+              columnId: parts[2],
+              componentId: parts[3],
+              data: {
+                text: editor.getContent()
+              }
+            });
+          });
+        }
+      };
+
+      tinymce.init(settings);
+      tinymce.execCommand('mceFocus', false, editorId);
     }
   }
 </script>
