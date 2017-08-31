badfb90a40d534e27911240cbf9b23e613abe58f
Emiliano Mongelo <emiliano@stensul.com>
2017-08-31 17:55:24 -0300

    [SV2] Separate Editmodule.vue into mutiple partials

---
+++ resources/assets/vue/components/admin/partials/ColumnSettings.vue
@@ -0,0 +1,246 @@
+<template>
+  <div>
+    <b-btn block v-b-toggle.column-settings class="module-settings-item">
+      <p class="pull-left"><i class="glyphicon glyphicon-pause"></i> COLUMN SETTINGS</p>
+      <i class="glyphicon glyphicon-menu-down menu-dropdown pull-right"></i>
+    </b-btn>
+
+    <b-collapse id="column-settings" accordion="module-settings-accordion">
+      <b-card class="control container-fluid" no-block>
+        <b-tabs card ref="tabs" v-model="tabIndex">
+          <!-- Render Tabs -->
+
+          <b-tab :title="`${key+1}`"
+                 :button-id="`column-${key}`"
+                 :key="key"
+                 v-for="(column, key) in module.structure.columns"
+          >
+            <div class="row" :class="'field-' + columnSetting.name" v-for="(columnSetting, keySettings ) in column.settings">
+              <div v-if="!columnSetting.group" >
+                <label class="col-sm-8 control-label" :for="columnSetting.name">{{ columnSetting.label }}</label>
+                <div class="col-sm-4 position-relative content-colorpicker">
+
+                  <!-- Input Text -->
+                  <input v-if="columnSetting.type === 'text'"
+                         v-model="columnSetting.value"
+                         v-validate="'required'"
+                         :class="{'input': true, 'is-danger': errors.has(columnSetting.name) }"
+                         :name="columnSetting.name"
+                         :placeholder="columnSetting.label"
+                         type="text"
+                         @change="saveColumnSettings(key)">
+
+                  <!-- Input color -->
+                  <input v-if="columnSetting.type === 'color'"
+                         v-model="columnSetting.value.hex"
+                         v-validate="'required'"
+                         class="sketchbackground"
+                         :class="{'input': true, 'is-danger': errors.has(columnSetting.name) }"
+                         :name="columnSetting.name"
+                         :placeholder="columnSetting.label"
+                         type="text"
+                         @click.prevent="toggleSketch"
+                         @change="saveColumnSettings(key)">
+                  <div v-if="columnSetting.type === 'color'"
+                       class="icon-remove st-remove-sketch"
+                       @click.prevent="toggleSketch">
+                    <i class="glyphicon glyphicon-remove"></i>
+                  </div>
+                  <sketch-picker v-if="columnSetting.type === 'color'"
+                                 v-model="columnSetting.value"
+                                 class="sketch-picker"
+                                 @click.native="updateColumnSettings(key, columnSetting.name, columnSetting.link, false )"></sketch-picker>
+
+                  <!-- Span General Error -->
+                  <span v-show="errors.has(columnSetting.name)"
+                        class="help is-danger">{{ errors.first(columnSetting.name) }}
+                  </span>
+                </div>
+              </div>
+
+              <div v-else>
+                <label class="col-sm-4 control-label" :for="columnSetting.name">{{ columnSetting.label }}</label>
+                <div class="col-sm-3 pull-left row no-gutters input-group-setting position-relative content-colorpicker" v-for="(columnSettingGroup, keySettings) in columnSetting.group" >
+
+                 <!-- Input text -->
+                  <input v-if="columnSettingGroup.type === 'text'"
+                         :class="{'input': true, 'is-danger': errors.has(columnSettingGroup.name) }"
+                         :name="columnSettingGroup.name"
+                         v-model="columnSettingGroup.value"
+                         :placeholder="columnSettingGroup.label"
+                         type="text"
+                         v-validate="'required'"
+                         @change="saveColumnSettings(key)">
+                  <!-- Input select -->
+                  <div>
+                    <b-form-select
+                        v-if="columnSettingGroup.type === 'select'"
+                        v-model="columnSettingGroup.value"
+                        :name="columnSettingGroup.name"
+                        :options="optionsSelectedBorderStyle"
+                        @change.native="saveColumnSettings(key)">
+                    </b-form-select>
+                  </div>
+
+                  <!-- Input color -->
+                  <input v-if="columnSettingGroup.type === 'color'"
+                         v-model="columnSettingGroup.value.hex"
+                         v-validate="'required'"
+                         class="sketchborder"
+                         :class="{'input': true, 'is-danger': errors.has(columnSettingGroup.name) }"
+                         :name="columnSettingGroup.name"
+                         :placeholder="columnSettingGroup.label"
+                         type="text"
+                         @click.prevent="toggleSketch"
+                         @change="saveColumnSettings(key)">
+                  <div v-if="columnSettingGroup.type === 'color'"
+                       class="icon-remove st-remove-sketch"
+                       @click.prevent="toggleSketch">
+                    <i class="glyphicon glyphicon-remove"></i>
+                  </div>
+                  <sketch-picker v-if="columnSettingGroup.type === 'color'"
+                                 v-model="columnSettingGroup.value"
+                                 class="sketch-picker"
+                                 @click.native="updateColumnSettings(key, columnSettingGroup.name, columnSettingGroup.link, true  )"></sketch-picker>
+                  <!-- Span General Error -->
+                  <span v-show="errors.has(columnSettingGroup.name)"
+                        class="help is-danger">{{ errors.first(columnSettingGroup.name) }}
+                  </span>
+                </div>
+              </div>
+            </div>
+
+          </b-tab>
+        </b-tabs>
+      </b-card>
+    </b-collapse>
+    <!-- END: Module Settings -->
+    <!-- START: Elements -->
+    <b-btn block v-b-toggle.element class="module-settings-item">
+      <p class="pull-left"><i class="glyphicon glyphicon-th-large"></i> ELEMENTS</p>
+      <i class="glyphicon glyphicon-menu-down menu-dropdown pull-right"></i>
+    </b-btn>
+
+    <b-collapse id="element" accordion="module-settings-accordion">
+      <b-card class="control">
+        <draggable :element="'ul'"
+                   :options="options"
+                   width="100%"
+                   class="components-list"
+        >
+          <li class="component-item" data-type="text-element">
+            <i class="fa fa-align-justify"></i>
+            <p>Text</p>
+          </li>
+          <li class="component-item" data-type="image-element">
+            <i class="fa fa-picture-o" aria-hidden="true"></i>
+            <p>Image</p>
+          </li>
+          <li class="component-item" data-type="button-element">
+            <i class="fa fa-square" aria-hidden="true"></i>
+            <p>CTA</p>
+          </li>
+          <li class="component-item" data-type="divider-element">
+            <i class="fa fa-minus-square" aria-hidden="true"></i>
+            <p>Divider</p>
+          </li>
+        </draggable>
+      </b-card>
+    </b-collapse>
+  </div>
+</template>
+
+<script>
+
+  import _ from 'lodash';
+  import { Sketch } from 'vue-color';
+  import Draggable from 'vuedraggable';
+  import BootstrapVue from 'bootstrap-vue';
+
+  export default {
+    components: {
+      Draggable,
+      BootstrapVue,
+      'sketch-picker': Sketch,
+    },
+    computed: {
+      module() {
+        return this.$store.getters["module/module"];
+      },
+    },
+    data () {
+      return {
+        optionsSelectedBorderStyle: [
+          { value: 'solid', text: 'solid' },
+          { value: 'inherit', text: 'inherit' },
+          { value: 'initial', text: 'initial' },
+          { value: 'outset', text: 'outset' },
+          { value: 'inset', text: 'inset' },
+          { value: 'double', text: 'double' },
+          { value: 'dashed', text: 'dashed' },
+          { value: 'dotted', text: 'dotted' },
+          { value: 'hidden', text: 'hidden' },
+          { value: 'none', text: 'none' },
+        ],
+        tabIndex: null,
+        options: {
+          group:{
+            name:'componentsList',
+            pull: 'clone',
+            put: false,
+          },
+          sort: false,
+          ghostClass: "ghost-component-menu",  // Class name for the drop placeholder
+          chosenClass: "chosen-component-menu",  // Class name for the chosen item
+          dragClass: "drag-component-menu"  // Class name for the dragging item
+        },
+      }
+    },
+    methods: {
+      toggleSketch(e){
+        const inputElement = e.toElement;
+        $(inputElement).closest('.content-colorpicker').find('.sketch-picker, .st-remove-sketch').toggleClass('st-show-element');
+      },
+      saveColumnSettings(key) {
+        _.each(this.module.structure.columns[key].settings, (option, index) => {
+          if (option.link === 'style') {
+            if ( option.group && option.group.length > 0 ){
+              _.each(option.group, (optionGroup, indexGroup) => {
+                this.module.structure.columns[key].style[optionGroup.name] = optionGroup.value;
+              });
+            }else{
+              this.module.structure.columns[key].style[option.name] = option.value;
+            }
+          }
+          if (option.link === 'attribute') {
+            if (option.group && option.group.length > 0 ){
+              _.each(option.group, (optionGroup, indexGroup) => {
+                this.module.structure.columns[key].attribute[optionGroup.name] = optionGroup.value;
+              });
+            }else{
+              this.module.structure.columns[key].attribute[option.name] = option.value;
+            }
+          }
+        });
+      },
+      // TODO Update date used mutation.
+      updateColumnSettings( key , name, link , isGroup ){
+        _.each(this.module.structure.columns[key].settings, (option, index) => {
+
+            if ( isGroup ){
+               _.each(option.group, (optionGroup, indexGroup) => {
+                if (optionGroup.name === name) {
+                    this.module.structure.columns[key][link][name] = optionGroup.value.hex;
+                }
+              });
+            }else{
+              if (option.name === name) {
+                this.module.structure.columns[key][link][name] = option.value.hex;
+              }
+            }
+
+        });
+      },
+    },
+  }
+</script>
---
+++ resources/assets/vue/components/admin/partials/ModuleHeader.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="row header">
+    <div class="col-xs-3 header-col">
+      <div class="beta-btn-secondary pull-left" @click="$router.push('/')">
+        <i class="glyphicon glyphicon-menu-left"></i>
+          <a href="#" >Back</a>
+      </div>
+      <div class="col-xs-8 section-title vertical-center">New Module</div>
+    </div>
+
+    <div class="col-xs-6 header-col">
+      <div class="section-title vertical-center">
+        <div class="switch">
+          <input type="radio" class="switch-input" name="view" value="desktop" id="desktop" checked>
+          <label for="desktop" class="switch-label switch-label-off campaign-switch-view" @click="buildingMode = 'desktop'">
+            <i class="fa fa-desktop"></i>
+          </label>
+          <input type="radio" class="switch-input" name="view" value="mobile" id="mobile">
+          <label for="mobile" class="switch-label switch-label-on campaign-switch-view" @click="buildingMode = 'mobile'">
+            <i class="glyphicon glyphicon-phone"></i>
+          </label>
+          <span class="switch-selection"></span>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-xs-3 header-col">
+      <div class="vertical-center pull-right">
+        <a class="btn btn-continue beta-btn-secondary m-l-button" href="#" @click.prevent="toggleRaw">Raw</a>
+        <a class="btn btn-continue beta-btn-secondary m-l-button" href="#" @click.prevent="saveModule('draft')" :disabled="errors.any()">Save as draft<i class="glyphicon glyphicon-menu-right"></i></a>
+        <a class="btn btn-continue beta-btn-secondary m-l-button" href="#" @click.prevent="saveModule('publish')">Publish<i class="glyphicon glyphicon-menu-right"></i></a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    computed: {
+      module() {
+        return this.$store.getters["module/module"];
+      }
+    },
+    data () {
+      return {
+        showRaw: false,
+        buildingMode: 'desktop'
+      }
+    },
+    methods: {
+      toggleRaw() {
+        this.showRaw = !this.showRaw;
+      },
+      updateRawModule(e) {
+        this.$store.commit("module/setModuleData", JSON.parse(e.target.value));
+      },
+      setModuleField(data) {
+        this.$store.commit("module/setModuleFields", data);
+      },
+      saveModule(status) {
+        this.$store.commit("global/setLoader", true);
+        this.setModuleField({ status });
+
+        let data = this.module;
+
+        this.$store.dispatch("module/saveModuleData", data)
+          .then( response => {
+            if (!response) {
+              this.$root.$toast('Error', {className: 'et-error'});
+              this.$store.commit("global/setLoader", false);
+              return;
+            }
+
+            this.$store.commit("global/setLoader", false);
+            if (this.module.status === 'publish') {
+              this.$router.push('/');
+            } else if (this.module.status === 'draft' && data.moduleId) {
+              this.$router.push('/edit/' + data.moduleId);
+            }
+            this.$root.$toast('Module Saved', {className: 'et-success'});
+          }).catch( error => {
+            this.$store.commit("global/setLoader", false);
+            this.$root.$toast('Got nothing from server. Prompt user to check internet connection and try again', {className: 'et-error'});
+          });
+      },
+    }
+  }
+</script>
