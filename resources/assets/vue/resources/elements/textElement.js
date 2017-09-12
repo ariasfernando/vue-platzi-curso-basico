d63c1877c680271a0d731c44556d77be72b34e67
Andres Lopez <andres@fanbridge.com>
2017-09-12 13:50:13 -0300

    remove attribute ein element text

--- resources/assets/vue/resources/elements/textElement.js
+++ resources/assets/vue/resources/elements/textElement.js
@@ -2,7 +2,6 @@ module.exports = {
   type: 'text-element',
   text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
   style: {
-    textAlign: 'left',
     fontFamily: 'sans-serif',
     fontSize: '12px',
     fontWeight: 'normal',
