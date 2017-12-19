2c80aa666c05bc30accbdd1d1e9c563906a1e949
Andres Lopez <andres@fanbridge.com>
2017-12-19 17:09:22 -0300

    [SV2-148] create plugin heightVariable

---
+++ resources/assets/vue/plugins/modules/variable-height/index.js
@@ -0,0 +1,21 @@
+const studioSettings = require('./studioSettings.vue');
+const campaignSettings = require('./campaignSettings.vue');
+
+module.exports = {
+  name: 'variable-height',
+  title: 'Variable Height',
+  version: '0.0.1',
+  author: 'andres@stensul.com',
+  target: ['divider'],
+  studioSettings,
+  campaignSettings,
+  config: {
+    options: {
+      max: 100,
+      min: 20,
+    },
+  },
+  data: {},
+  render: true,
+  enabled: false,
+};
