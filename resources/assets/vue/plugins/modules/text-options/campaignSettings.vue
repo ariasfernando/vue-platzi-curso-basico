99166bc494591745dc3d5d21461326222f812e26
Emiliano Mongelo <emiliano@stensul.com>
2017-12-20 14:46:23 -0300

    [SV2] Fix

--- resources/assets/vue/plugins/modules/text-options/campaignSettings.vue
+++ resources/assets/vue/plugins/modules/text-options/campaignSettings.vue
@@ -64,7 +64,7 @@
           init_instance_callback: (editor) => {
 
             editor.on('blur', (e) => {
-              this.$store.dispatch('campaign/updateElement', {
+              this.$store.commit('campaign/updateElement', {
                 moduleId: this.currentComponent.moduleId,
                 columnId: this.currentComponent.columnId,
                 componentId: this.currentComponent.componentId,
