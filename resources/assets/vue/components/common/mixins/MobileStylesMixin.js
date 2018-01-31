fb43b7a859d0c9416af9201cab4d059076d11ba0
Matias Rios <matias@stensul.com>
2018-01-31 15:22:39 -0300

    [SV2-228] Add validation in mixin for retrocompability

--- resources/assets/vue/components/common/mixins/MobileStylesMixin.js
+++ resources/assets/vue/components/common/mixins/MobileStylesMixin.js
@@ -4,16 +4,19 @@ export default {
   methods: {
     // Get an array of classes matching the selector param in config (NOT in DOM or VDOM)
     getMobileClasses(component, selector) {
-      let arrayClassesToApply =
-      _.reduce(
-        // settings is an object, get properties as array
-        _.values(component.plugins.mobileStyles.config.settings),
-        (result, prop, key) => {
-          result.push((prop.value && prop.selector === selector) ? prop._class : '');
-          return result;
-        }, []
-      );
-      return _.join(arrayClassesToApply, ' ');
+      if(_.has('mobileStyles',component.plugins)) {
+        let arrayClassesToApply =
+        _.reduce(
+          // settings is an object, get properties as array
+          _.values(component.plugins.mobileStyles.config.settings),
+          (result, prop, key) => {
+            result.push((prop.value && prop.selector === selector) ? prop._class : '');
+            return result;
+          }, []
+        );
+        return _.join(arrayClassesToApply, ' ');
+      }
+      else return '';
     }
   },
 }
