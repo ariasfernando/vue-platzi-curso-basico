009b614dc3c71e850998c6473bb8fa4fc8a621b0
Emiliano Mongelo <emiliano@stensul.com>
2018-02-15 15:24:28 -0300

    [SV2] Fix tiny

--- resources/assets/vue/plugins/modules/text-options/campaignSettings.vue
+++ resources/assets/vue/plugins/modules/text-options/campaignSettings.vue
@@ -73,8 +73,8 @@
           paste_as_text:true,
           relative_urls: false,
           textcolor_rows: 1,
-          max_chars: this.plugin.config.settings.truncate.content,
-          max_lines: this.plugin.config.settings.lines_limit.content,
+          max_chars: this.plugin.config.settings.truncate ? this.plugin.config.settings.truncate.content : undefined,
+          max_lines: this.plugin.config.settings.lines_limit ? this.plugin.config.settings.lines_limit.content : undefined,
 
           init_instance_callback: (editor) => {
 
