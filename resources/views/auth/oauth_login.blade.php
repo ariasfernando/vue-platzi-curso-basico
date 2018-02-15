9fa58462b53eb5638cdf37d85f2c9ea06329fc18
Maxi Di Carlo <maxi@fanbridge.com>
2018-02-15 17:16:28 -0300

    [SV2-329]

--- resources/views/auth/oauth_login.blade.php
+++ resources/views/auth/oauth_login.blade.php
@@ -10,7 +10,7 @@
     <section class="col-md-4 col-sm-10 col-xs-10 beta-login">
 
         <div class="text-center">
-            <a href="https://stensul.com" class="stensul-logo open-sans" target="_blank">stensul</a>
+            <a href="/" class="stensul-logo open-sans" target="_blank">stensul</a>
             <h4>
                 Email Creator
             </h4>
