82c7ffc3992ec29651dc02a88f40609406e7a041
Luciano Zanetta <luciano@Lucianos-MacBook-Pro.local>
2018-02-23 18:27:30 -0300

    [SV2-364] refactored studio ButtonCaret setting component

--- resources/assets/vue/components/admin/settings/ButtonCaret.vue
+++ resources/assets/vue/components/admin/settings/ButtonCaret.vue
@@ -9,87 +9,79 @@
 </template>
 
 <script>
+import SettingMixin from "../mixins/SettingMixin.js";
 
-  export default {
-    name: 'ButtonCaret',
-    props: ['setting'],
-    computed: {
-      currentComponent() {
-        return this.$store.getters["module/currentComponent"];
-      },
-      buttonCaret() {
-        const module = this.$store.getters["module/module"];
-        const component = module.structure.columns[this.currentComponent.columnId].components[this.currentComponent.componentId];
-
-        return component.attribute.buttonCaret;
-      },
+export default {
+  name: "ButtonCaret",
+  props: ["setting"],
+  mixins: [ SettingMixin ],
+  data() {
+    return {
+      name: "buttonCaret"
+    };
+  },
+  computed: {
+    buttonCaret() {
+      return this.component.attribute[this.name];
+    }
+  },
+  methods: {
+    resetImage() {
+      this.$emit("attribute-setting-updated", { name: this.name, value: "" });
     },
-    methods: {
-      resetImage() {
-        this.$store.commit('module/saveComponentAttribute',{
-          columnId: this.currentComponent.columnId,
-          componentId: this.currentComponent.componentId,
-          attribute: 'buttonCaret',
-          attributeValue: ''
-        });
-      },
-      onFileChange(e) {
-        const files = e.target.files || e.dataTransfer.files;
+    onFileChange(e) {
+      const files = e.target.files || e.dataTransfer.files;
 
-        if (!files.length)
-          return;
+      if (!files.length) return;
 
-        this.createImage(files[0]);
-      },
-      createImage(file) {
-        const reader = new FileReader();
-        const vm = this;
+      this.createImage(files[0]);
+    },
+    createImage(file) {
+      const reader = new FileReader();
+      const vm = this;
 
-        reader.onload = (e) => {
-          vm.image = e.target.result;
+      reader.onload = e => {
+        vm.image = e.target.result;
 
-          // Upload Image
-          this.$store.dispatch('module/uploadImages', {
-            images: [ vm.image ],
-          }).then((res) => {
-            this.updateAttributePlaceholder('customer/modules' + res[0]);
+        // Upload Image
+        this.$store
+          .dispatch("module/uploadImages", {
+            images: [vm.image]
+          })
+          .then(res => {
+            this.updateAttributePlaceholder("customer/modules" + res[0]);
           });
-        };
+      };
 
-        reader.readAsDataURL(file);
-      },
-      updateAttributePlaceholder(e) {
-        // Set the src after we have loaded the new image
-        const tmp = new Image();
-        tmp.src = this.$_app.config.imageUrl + e;
+      reader.readAsDataURL(file);
+    },
+    updateAttributePlaceholder(e) {
+      // Set the src after we have loaded the new image
+      const tmp = new Image();
+      tmp.src = this.$_app.config.imageUrl + e;
 
-        tmp.onload = () => {
-          this.$store.commit('module/saveComponentAttribute', {
-            columnId: this.currentComponent.columnId,
-            componentId: this.currentComponent.componentId,
-            attribute: 'buttonCaret',
-            attributeValue: e
-          });
-        };
+      tmp.onload = () => {
+        this.$emit("attribute-setting-updated", { name: this.name, value: e });
+      };
 
-        tmp.onerror = () => {
-          // Retry to load image
-          this.updateAttributePlaceholder(e);
-        };
-      },
+      tmp.onerror = () => {
+        // Retry to load image
+        this.updateAttributePlaceholder(e);
+      };
     }
   }
+};
 </script>
 
 <style lang="less" scoped>
-  .field-button-caret {
-    input {
-      display: inline;
-      width: 80px !important;
-    }
-
-    i.st-remove {
-      cursor: pointer;
-    }
+.field-button-caret {
+  input {
+    display: inline;
+    width: 80px !important;
   }
+
+  i.st-remove {
+    cursor: pointer;
+  }
+}
 </style>
