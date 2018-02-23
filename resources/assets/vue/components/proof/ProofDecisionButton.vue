400c3c65bc3ce99d69c10e93e0a838b9468f6a81
Alejandro <alejandro@stensul.com>
2018-02-23 12:13:13 -0300

    [APPROVALS-25] fix decision buttons

--- resources/assets/vue/components/proof/ProofDecisionButton.vue
+++ resources/assets/vue/components/proof/ProofDecisionButton.vue
@@ -3,12 +3,13 @@
         :class="buttonClasses"
         :disabled="loadingDecision.length > 0"
         v-if="selectedDecision.length === 0 || selectedDecision === decision"
+        :title="selectedDecision.indexOf('-with-comments') + ' ' + selectedDecision"
         @click="submitDecision()"
         @mouseover="undo = (selectedDecision === decision)"
         @mouseleave="undo = false">
             <i :class="['glyphicon glyphicon-' + icon, {hidden: undo}]"></i>
             {{ formatedTitle }}
-            <i class="glyphicon glyphicon-remove remove-decision" :class="{hidden: !undo}"></i>
+            <i class="glyphicon glyphicon-remove remove-decision pull-right" :class="{hidden: !undo}"></i>
     </button>
 </template>
 
@@ -16,20 +17,21 @@
     module.exports = {
         name: 'proofDecisionButton',
         props: ['decision', 'icon', 'selectedDecision', 'loadingDecision', 'title'],
-        data: function() {
+        data () {
             return {
                 undo: false
             }
         },
         computed: {
-            buttonClasses: function() {
+            buttonClasses () {
                 return {
                     'ajax-loader-small': this.loadingDecision === this.decision,
                     'approved': this.selectedDecision === 'approve' || this.selectedDecision === 'approve-with-comments',
-                    'rejected': this.selectedDecision === 'reject' || this.selectedDecision === 'reject-with-comments'
+                    'rejected': this.selectedDecision === 'reject' || this.selectedDecision === 'reject-with-comments',
+                    'with-comment': this.selectedDecision.indexOf('-with-comments') > -1
                 }
             },
-            formatedTitle: function() {
+            formatedTitle () {
                 if (this.undo === true) {
                     return 'Undo decision';
                 }
@@ -37,10 +39,10 @@
                     return this.title.replace('Approve', 'Approved').replace('Reject', 'Rejected');
                 }
                 return this.title;
-            },
+            }
         },
         methods: {
-            submitDecision: function() {
+            submitDecision () {
                 if (this.undo === true) {
                     this.$emit('undo-decision');
                 } else {
@@ -51,18 +53,22 @@
     }
 </script>
 
-<style lang="sass">
+<style lang="less" scoped>
     .approved {
         background: #daeadd;
         border: none;
         color: green;
-    },
+    }
     .rejected {
         background: #fcded9;
         border: none;
         color: red;
+        width: 130px;
     }
     .remove-decision {
         padding-left: 10px;
+    }
+    .with-comment {
+        width: 190px;
     }
 </style>
