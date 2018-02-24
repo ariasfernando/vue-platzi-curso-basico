b9d7c51a98ccdaad6ffc87660e6c571c83381fa5
Maxi Di Carlo <maxi@fanbridge.com>
2018-02-24 19:15:35 -0300

    [MS-129] icon align

--- resources/assets/vue/components/dashboard/DraftEmails.vue
+++ resources/assets/vue/components/dashboard/DraftEmails.vue
@@ -90,7 +90,7 @@
                 href="#"
                 data-tooltip="Send for review"
                 v-if="proof.allow && proof.status"
-                ><i class="glyphicon glyphicon-search"></i></a>
+                ><i class="glyphicon glyphicon-check"></i></a>
 
               <a
                 :href="$_app.config.baseUrl + '/campaign/edit/' + campaign._id"
