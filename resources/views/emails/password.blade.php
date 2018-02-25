363131522bf2bfc0599b5f402323a53b307c7bcf
Gabriel Sosa <gabriel@stensul.com>
2018-02-24 21:38:19 -0300

    [SV2-390] updated forgot email and subject

--- resources/views/emails/password.blade.php
+++ resources/views/emails/password.blade.php
@@ -1,4 +1,15 @@
-Click here to reset your password: <a href="{{ url('password/reset/'.$token) }}" target="_blank">{{ url('password/reset/'.$token) }}</a><br/><br/>
+Hi {{ $name }},
 
-If you did not request a password reset, please email us immediately at <a href="mailto:{{$app_config['app_mail_address']}}">{{$app_config['app_mail_address']}}</a> <br/>
-This link expires in one hour.
+We received a request to reset the password for your stensul account (<a href="{{ url('/') }}" target="_blank">{{ url('/') }}</a>). <br/>
+
+To reset your password, please go to:<br />
+<a href="{{ url('password/reset/'.$token) }}" target="_blank">{{ url('password/reset/'.$token) }}</a><br/><br/>
+
+This link expires in one hour. <br/>
+
+If you did not request a password reset, please email us immediately at <a href="mailto:{{ $app_config['app_mail_address'] }}">{{ $app_config['app_mail_address'] }}</a> <br/>
+
+How do I know an email is from stensul? <br/>
+The link in this email will start with "https://” and contain “stensul.com” <br/>
+
+stensul, Inc. | 150 West 25th Street, 3rd Floor, New York, NY 10001
