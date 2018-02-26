a24977aee4d68819e27b32519f2bf1c9e4f9f219
Manuel Zambrano <manuel.zambrano@stensul.com>
2018-02-26 20:15:27 -0300

    [SV2-413] Now width depends on campaign configurations

--- resources/assets/vue/utils/hackMediaQuery.js
+++ resources/assets/vue/utils/hackMediaQuery.js
@@ -1,4 +1,4 @@
-export default function (selector) {
+export default function (selector, width) {
     let $canvas = null;
 
     $canvas = document.querySelector(selector);
@@ -6,10 +6,10 @@ export default function (selector) {
     if ($canvas.querySelector('td').querySelector('table').querySelector('td.st-hide-hack') === null) {
         let $hack = $(`<tr>
             <td class="st-hide-hack">
-                <table cellpadding="0" cellspacing="0" border="0" align="center" width="660">
+                <table cellpadding="0" cellspacing="0" border="0" align="center" width="${width}">
                     <tr>
-                        <td cellpadding="0" cellspacing="0" border="0" height="1" style="background-color:#ffffff; line-height:1px; height: 1px; min-width: 660px;">
-                            <img src="${Application.globals.baseUrl}/images/spacer.gif" height="1" width="660" style="max-height:1px; min-height:1px; display:block; width:660px; min-width:660px; border: 0;"/>
+                        <td cellpadding="0" cellspacing="0" border="0" height="1" style="background-color:#ffffff; line-height:1px; height: 1px; min-width: ${width}px;">
+                            <img src="${Application.globals.baseUrl}/images/spacer.gif" height="1" width="${width}" style="max-height:1px; min-height:1px; display:block; width:${width}px; min-width:${width}px; border: 0;"/>
                         </td>
                     </tr>
                 </table>
