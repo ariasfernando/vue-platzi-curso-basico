fda272dbb23f591c4466f35ecd07817691434678
Alejandro <alejandro@stensul.com>
2018-03-24 23:06:58 -0300

    [SV2-533] update images path in modules

--- database/migrations/2018_03_24_195123_update_studio_images_path.php
+++ database/migrations/2018_03_24_195123_update_studio_images_path.php
@@ -21,6 +21,22 @@ class UpdateStudioImagesPath extends Migration
         // Make it faster by disabling query log.
         DB::connection()->disableQueryLog();
 
+        $this->updateImagesPathInCampaigns();
+        $this->updateImagesPathInModules();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+
+    protected function updateImagesPathInCampaigns()
+    {
         /*
          * Iterate through all campaigns
          * Get a bunch of campaigns at a time to save roundtrip times to the server.
@@ -76,13 +92,59 @@ class UpdateStudioImagesPath extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+    protected function updateImagesPathInModules()
     {
-        //
+        /*
+         * Iterate through all modules
+         * Get a bunch of modules at a time to save roundtrip times to the server.
+        */
+        Module::withTrashed()->chunk(100, function ($modules) {
+
+            foreach ($modules as $module) {
+
+                Logging::info('Module ' . $module->id);
+
+                $module_structure = $module->structure;
+                if (isset($module_structure['columns'])) {
+                    foreach ($module_structure['columns'] as $column_key => $column) {
+                        if (isset($column['components'])) {
+                            foreach ($column['components'] as $component_key => $component_value) {
+                                if (isset($component_value['type'])) {
+                                    switch ($component_value['type']) {
+                                        case 'button-element':
+                                            if (isset($component_value['buttonCaret']) && isset($component_value['buttonCaret']['attribute'])) {
+                                                if (isset($component_value['buttonCaret']['attribute']['url'])) {
+                                                    if (strpos($component_value['buttonCaret']['attribute']['url'], 'customer/modules/') !== false) {
+                                                        $new_value = str_replace('customer/modules/', 'customer/modules/studio/', $component_value['buttonCaret']['attribute']['url']);
+                                                        $module_structure['columns'][$column_key]['components'][$component_key]['buttonCaret']['attribute']['url'] = $new_value;
+                                                    }
+                                                }
+                                            }
+                                            break;
+                                        case 'image-element':
+                                            if (isset($component_value['attribute']) && $component_value['attribute']['placeholder']) {
+                                                if (strpos($component_value['attribute']['placeholder'], 'customer/modules/') !== false) {
+                                                    $new_value = str_replace('customer/modules/', 'customer/modules/studio/', $component_value['attribute']['placeholder']);
+                                                    $module_structure['columns'][$column_key]['components'][$component_key]['attribute']['placeholder'] = $new_value;
+                                                }
+                                            }
+                                            break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                $module->structure = $module_structure;
+
+                /*
+                 * Test and compare modules_data before and after and then
+                 * uncomment the following line to save the module.
+                */
+                $module->timestamps = false;
+                $module->save();
+            }
+        });
     }
 }
