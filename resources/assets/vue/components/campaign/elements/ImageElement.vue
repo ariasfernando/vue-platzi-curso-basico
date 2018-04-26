c854704b072d87c46c7f105d0f825f1b88242749
Facundo García Juliá <facundo.garcia@stensul.com>
2018-04-26 12:16:08 -0300

    [GAR-94] Fix image height of img

--- resources/assets/vue/components/campaign/elements/ImageElement.vue
+++ resources/assets/vue/components/campaign/elements/ImageElement.vue
@@ -43,7 +43,7 @@
                   border="0"
                   :width="component.image.attribute.width" 
                   :src="imageUrl(component.image.attribute.placeholder)"
-                  :height="component.image.attribute.height"
+                  :height="component.image.attribute.height === 'auto' ? undefined : component.image.attribute.height"
                   :alt="component.image.attribute.alt"
                   :title="component.image.attribute.title"
                 >
@@ -56,7 +56,7 @@
                       class="st-resize"
                       style="display:block;border:none;max-width:100%;height:auto;"
                       :width="component.image.attribute.width" 
-                      :height="component.image.attribute.height"
+                      :height="component.image.attribute.height === 'auto' ? undefined : component.image.attribute.height"
                       :alt="component.image.attribute.alt"
                       :title="component.image.attribute.title"
                     />
