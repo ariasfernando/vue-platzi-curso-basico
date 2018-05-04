f106c76ebd7d0271a5292031f5ec6592dbfce6de
Emiliano Mongelo <emiliano@stensul.com>
2018-05-04 12:02:45 -0300

    [SV2-586] Other fix

--- resources/assets/vue/components/dashboard/FinishedEmails.vue
+++ resources/assets/vue/components/dashboard/FinishedEmails.vue
@@ -80,7 +80,7 @@
               <a
                 href="#"
                 class="lock-campaign"
-                v-if="enableLocking && !campaign.locked"
+                v-if="$can('fix_layout') && (enableLocking && !campaign.locked)"
                 v-on:click.prevent="lockCampaign(campaign._id, campaigns.current_page)"
                 data-toggle="tooltip"
                 data-placement="bottom"
