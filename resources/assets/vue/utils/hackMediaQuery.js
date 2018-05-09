e48d49fc4e2edc8abf988a7cf17fc44ddcdbc28e
Facundo García Juliá <facundo.garcia@stensul.com>
2018-05-09 17:57:37 -0300

    [SV2-598] spacer.gif moved

--- resources/assets/vue/utils/hackMediaQuery.js
+++ resources/assets/vue/utils/hackMediaQuery.js
@@ -9,7 +9,7 @@ export default function (selector, width) {
                 <table cellpadding="0" cellspacing="0" border="0" align="center" width="${width}">
                     <tr>
                         <td cellpadding="0" cellspacing="0" border="0" height="1" style="background-color:#ffffff; line-height:1px; height: 1px; min-width: ${width}px;">
-                            <img src="${Application.globals.baseUrl}/images/spacer.gif" height="1" width="${width}" style="max-height:1px; min-height:1px; display:block; width:${width}px; min-width:${width}px; border: 0;"/>
+                        <img src="${Application.globals.baseUrl}/_common/images/en_us/spacer.gif" height="1" width="${width}" style="max-height:1px; min-height:1px; display:block; width:${width}px; min-width:${width}px; border: 0;"/>
                         </td>
                     </tr>
                 </table>
