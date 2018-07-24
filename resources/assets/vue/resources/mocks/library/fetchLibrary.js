08cb16977116b4f7be8eab4cb0ea65e341413e30
Daniel Villalba <daniel@stensul.com>
2018-07-24 15:59:32 -0300

    [SV2-858] correction keys and value

--- resources/assets/vue/resources/mocks/library/fetchLibrary.js
+++ resources/assets/vue/resources/mocks/library/fetchLibrary.js
@@ -1,12 +1,12 @@
 export default [
   {
     _id: '5b2c12d557ea5300186a0ca2',
-    name: 'prueba',
-    key: 'prueba',
+    name: 'test',
+    key: 'test any',
   },
   {
     _id: '5b2c13fb57ea5300131ad6e2',
-    name: 'pruebba',
-    key: 'pruebba',
+    name: 'test',
+    key: 'test any',
   },
 ];
