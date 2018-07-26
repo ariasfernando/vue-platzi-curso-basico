a699631b7e312de4dab9628a874db11d226c7c0b
Manuel Zambrano <manuel.zambrano@stensul.com>
2018-07-26 17:12:57 -0300

    Revert "[ADMIN-76] Links at the top similar to v1 to toggle between  the editor dashboard and the admin tool added"
    This reverts commit b2115888eb8ec369333bb1ea8085172c1f7ec6cf.

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -33,13 +33,13 @@
                                 </ul>
                             </div>
                         @endif
-                        @if (Auth::user() && Auth::user()->can('access_admin'))
-                            <a href="{{ url('/admin')}}" class="help dropdown-toggle help-dropdown-menu">
-                                <i class="glyphicon glyphicon-cog"></i>
-                                <span>Admin</span>
-                            </a>
-                        @endif
 
+                    @endif
+                    @if (config('app.home.enabled') && (!Auth::check() || Auth::user()->can('access_dashboard')))
+                        <a href="{{ url('/') }}" class="home">
+                            <i class="fa fa-home"></i>
+                            <span>Home</span>
+                        </a>
                     @endif
                     @if (!Auth::guest())
                         <a href="{{ url('/auth/logout') }}" class="logout">
