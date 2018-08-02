52a5b38fdc78de8bc0c70a9db34e3827c7ae3714
Maxi Di Carlo <maxi@fanbridge.com>
2018-08-02 13:12:50 -0300

    studio central panel

--- resources/assets/vue/components/common/ElementSelector.vue
+++ resources/assets/vue/components/common/ElementSelector.vue
@@ -1,12 +1,12 @@
 <template>
   <button class="element-selector" @click="e => $emit('element-selected', e)">
-    {{label}}
+    <i :class="selectorIcon"  aria-hidden="true"></i> {{label}}
   </button>
 </template>
 
 <script>
   export default {
-    props: ['label'],
+    props: ['label', 'selectorIcon'],
     name: 'element-selector',
   };
   
@@ -14,6 +14,22 @@
 
 <style lang="scss" scoped>
 .element-selector {
-  
+  border-radius: 2px;
+  background: #CDCDCD;
+  border: none;
+  position: relative;
+  display: block;
+  margin: 0 auto;
+  margin-top: 20px;
+  font-size: 11px;
+  font-weight: normal;
+  color: #ffffff;
+  outline: none;
+  transition: all 0.3s linear;
+  padding: 2px 7px 3px 7px;
+
+  &:hover{
+    background: #aaaaaa;
+  }
 }
 </style>
