3f889ee1545e85586f5721fc3bb9a0d386230173
Facundo García Juliá <facundo.garcia@stensul.com>
2018-08-27 18:09:31 -0300

    [STD-145] Add styles to Json module

--- resources/assets/vue/components/admin/EditModule.vue
+++ resources/assets/vue/components/admin/EditModule.vue
@@ -10,7 +10,8 @@
         <!-- START: Module Container -->
         <div class="col-xs-8 module-container" @mouseup="clickModuleContainer">
           <div v-if="showRaw" class="module-wrapper">
-            <textarea v-html="module" @change="updateRawModule" rows="30" style="width: 100%"></textarea>
+            <code-editor v-model="moduleRow">
+            </code-editor>
           </div>
           <div v-else class="module-wrapper" :class="`stx-${buildingMode}-mode`">
             <module></module>
@@ -38,6 +39,7 @@ import ComponentSettings from "./ComponentSettings.vue";
 import ElementsSettings from "./partials/ElementsSettings.vue";
 import GeneralSettings from "./partials/GeneralSettings.vue";
 import Module from "./Module.vue";
+import CodeEditor from "./CodeEditor.vue";
 import ModuleHeader from "./partials/ModuleHeader.vue";
 import moduleService from "../../services/module";
 import Spinner from "../common/Spinner.vue";
@@ -45,6 +47,7 @@ import Spinner from "../common/Spinner.vue";
 export default {
   name: "EditModule",
   components: {
+    CodeEditor,
     ColumnBarContainer,
     ColumnSettings,
     ComponentSettings,
@@ -57,6 +60,14 @@ export default {
   computed: {
     module() {
       return this.$store.getters["module/module"];
+    },
+    moduleRow: {
+      get(){
+        return this.module;
+      },
+      set(values) {
+        this.$store.commit("module/setModuleData", JSON.parse(values));
+      },
     },
     currentComponent() {
       return this.$store.getters["module/currentComponent"];
@@ -138,9 +149,6 @@ export default {
 
       const sideToggled = document.getElementById("edit-container");
       sideToggled.classList.toggle("sidebar-closed");
-    },
-    updateRawModule(e) {
-      this.$store.commit("module/setModuleData", JSON.parse(e.target.value));
     },
     clickModuleContainer(e) {
       if($(e.target).hasClass('module-container')){
