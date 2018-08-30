22673914823008589c7d330a89b56bb4f4f9aa5a
Ximena Garcia <ximena.garcia@stensul.com>
2018-08-30 14:55:05 -0300

    Fix console error

--- resources/assets/vue/components/common/ImageModal/index.vue
+++ resources/assets/vue/components/common/ImageModal/index.vue
@@ -184,13 +184,13 @@ export default {
         sieOptions.preset = sieHelper.completeUrlPath(this.$_app.config.imageUrl, sieOptions.preset);
       }
 
-      this.overlayImages = this.overlayImages || [];
+      const overlayImages = this.overlayImages || [];
       
-      if(this.overlayImages.length > 0){
+      if(overlayImages.length > 0){
         const i = sieOptions.preset.find(e => {
           return e.type === 'sie-plugin-image-overlay';
         });
-        _.set(i, 'options.plugin.image.gallery.images', this.overlayImages);
+        _.set(i, 'options.plugin.image.gallery.images', overlayImages);
       }
 
       this.sieOptions = sieOptions;
