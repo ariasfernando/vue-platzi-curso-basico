612695717963eceaecd41a848ee10574d391b7ef
Elias Torres <elias@Eliass-MacBook-Pro.local>
2018-09-25 18:29:24 -0300

    [STD-240] added fix to valign inside vrect and regex to update background src

--- resources/assets/vue/components/common/BackgroundImage.vue
+++ resources/assets/vue/components/common/BackgroundImage.vue
@@ -42,10 +42,17 @@ export default {
                     <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="true" strokecolor="none" style="width:${this.convertPxToPt(this.width)}; height:${this.convertPxToPt(this.element.attribute.height)};" stroke="false">
                     <v:fill type="frame" src="${this.element.style.backgroundImage}" ${this.MsoBgcolor} />
                     <v:textbox inset="0,0,0,0">
+                      <table width="100%" cellpadding="0" cellspacing="0" border="0" style="width: 100%;">
+                        <tbody>
+                          <tr>
+                            <td width="100%" style="height:${this.convertPxToPt(this.element.attribute.height)};" valign="${this.element.attribute.valign}">
                   <![endif]-->`;
     },
     msoEndingComment() {
       return `<!--[if gte mso 9]>
+                          </td>
+                        </tr>
+                      </table>
                     </v:textbox>
                     </v:rect>
                   <![endif]-->`;
