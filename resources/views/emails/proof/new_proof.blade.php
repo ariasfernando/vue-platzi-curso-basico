98d3fede13fdb42a6d157e1c454eaa813bea4032
Alejandro Alvaro <alejandro@stensul.com>
2018-10-22 17:12:24 -0300

    [APPROVALS-90] fix on new_proof email

--- resources/views/emails/proof/new_proof.blade.php
+++ resources/views/emails/proof/new_proof.blade.php
@@ -13,21 +13,11 @@
                 <p style="font-family:'Open Sans', Arial, Helvetica, sans-serif; font-size:15px; color:#666666; font-weight:300; line-height:22px; -webkit-text-size-adjust:none;">Message from {{ $params['requestor'] }} to all reviewers: </p>
                 <p style="font-family:'Open Sans', Arial, Helvetica, sans-serif; font-size:15px; color:#666666; font-weight:300; line-height:22px; -webkit-text-size-adjust:none;">{{ $params['notification_message_to_all'] }}</p>
               @endif
-              @if (strlen($params['notification_message']) && !isset($params['reviewer']['notified']))
+              @if (strlen($params['notification_message']) && (!isset($params['reviewer']['notified']) || $params['reviewer']['notified'] === false))
                 <p style="font-family:'Open Sans', Arial, Helvetica, sans-serif; font-size:15px; color:#666666; font-weight:300; line-height:22px; -webkit-text-size-adjust:none;">Message directly to you:</p>
                 <p style="font-family:'Open Sans', Arial, Helvetica, sans-serif; font-size:15px; color:#666666; font-weight:300; line-height:22px; -webkit-text-size-adjust:none;">{{ $params['notification_message'] }}</p>
               @endif
             </td>
         </tr>
     @endif
-    <tr>
-        <td align="left" valign="top" style="font-family:'Open Sans', Arial, Helvetica, sans-serif; font-size:15px; color:#666666; font-weight:300; line-height:22px; -webkit-text-size-adjust:none;">
-            <br>
-            <p>To ensure important emails like this one reach your inbox, please add <a href="mailto:{{ $from_email }}">{{ $from_email }}</a> to your address book or safe sender list.</p>
-            <p>If you received this email in error, please contact stensul at <a href="mailto:{{ $app_config["app_mail_address"] }}">{{ $app_config["app_mail_address"] }}</a> immediately.</p>
-        </td>
-    </tr>
-    <tr>
-        <td height="24" align="left" valign="top"></td>
-    </tr>
 @endsection
