971b4443a33397990a7c3840b267ac471c823017
Manuel Zambrano <manuel.zambrano@stensul.com>
2018-11-14 17:35:26 -0300

    [SV2-1138] Check first if there's a target

--- resources/assets/vue/plugins/modules/toggle-element/logic.js
+++ resources/assets/vue/plugins/modules/toggle-element/logic.js
@@ -107,7 +107,7 @@ export default {
     applyLogicUpdates(settings) {
       let applyLogicUpdates = false;
 
-      if (_.isEmpty(settings.ids)) {
+      if (_.isEmpty(settings.ids) && typeof settings.target !== 'undefined') {
         applyLogicUpdates = true;
       } else if (typeof settings.target !== 'undefined' && _.indexOf(settings.ids, settings.target.elementId) !== -1) {
         // if ids array is not empty, only apply logic if elementId matches and id in ids
