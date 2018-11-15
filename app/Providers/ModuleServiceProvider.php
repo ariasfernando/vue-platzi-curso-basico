2cdf57b5b108a7a563b44338542703dfdcfe0f4c
Juan Ferrari <juan@stensul.com>
2018-11-15 16:16:55 -0300

    STD-392 add optional type parameter, migrations down fix.

--- app/Providers/ModuleServiceProvider.php
+++ app/Providers/ModuleServiceProvider.php
@@ -46,39 +46,44 @@ class ModuleServiceProvider extends ServiceProvider
     * Get all modules config.
     *
     * @param string $status Module status
+    * @param string $type Module type, null, "custom", "studio"
     * @return array
     */
-    public static function getModuleList($status = null)
+    public static function getModuleList($status = null, $type = null)
     {
          $modules = [];
 
-         // Load from module folder
-        $files = \File::allFiles(self::$module_dir);
+        if (is_null($type) || $type === 'custom') {
+            // Load from module folder
+            $files = \File::allFiles(self::$module_dir);
 
-        foreach ($files as $file) {
-            if ($file->isFile() && $file->getFilename() === 'config.json') {
-                $config = json_decode(file_get_contents($file->getPathName()));
-                $module_key = $config->key;
-                if (isset($config->enabled) && $config->enabled === true) {
-                    $modules[$module_key] = $config;
+            foreach ($files as $file) {
+                if ($file->isFile() && $file->getFilename() === 'config.json') {
+                    $config = json_decode(file_get_contents($file->getPathName()));
+                    $module_key = $config->key;
+                    if (isset($config->enabled) && $config->enabled === true) {
+                        $modules[$module_key] = $config;
+                    }
                 }
             }
         }
 
-        // Load modules form Db.
-        switch ($status) {
-            case 'publish':
-                $modules_db = Module::published()->get();
-                break;
-            case 'draft':
-                $modules_db = Module::draft()->get();
-                break;
-            default:
-                $modules_db = Module::all();
-        }
+        if (is_null($type) || $type === 'studio') {
+            // Load modules form Db.
+            switch ($status) {
+                case 'publish':
+                    $modules_db = Module::published()->get();
+                    break;
+                case 'draft':
+                    $modules_db = Module::draft()->get();
+                    break;
+                default:
+                    $modules_db = Module::all();
+            }
 
-        foreach ($modules_db as $module) {
-            $modules[$module->key] = $module;
+            foreach ($modules_db as $module) {
+                $modules[$module->key] = $module;
+            }
         }
 
         ksort($modules);
