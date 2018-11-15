4a1e67b490b7ae6d3a6cfa5976c613c2cd2cc5de
Facundo García Juliá <facundo.garcia@stensul.com>
2018-11-15 17:41:18 -0300

    [STD-386] Update migration log

--- database/migrations/2018_11_07_120453_update_buttons_auto_width_fix.php
+++ database/migrations/2018_11_07_120453_update_buttons_auto_width_fix.php
@@ -40,11 +40,11 @@ class UpdateButtonsAutoWidthFix extends Migration
             foreach ($campaigns as $campaign) {
                 $modules_data = $campaign["modules_data"];
                 foreach ($modules_data as $key => $module) {
+                    Logging::info('It will check '.$module['key']);
                     if (isset($module['structure']) && isset($module['structure']['columns'])) {
                         foreach ($module['structure']['columns'] as $column_key => $column) {
                             if (isset($column['components'])) {
                                 foreach ($column['components'] as $component_key => $component) {
-                                    Logging::info('It will check '.$module['key']);
                                     if (isset($component['type'])
                                         && ($component['type'] === 'button-element')
                                         && isset($component['button'])
@@ -86,11 +86,11 @@ class UpdateButtonsAutoWidthFix extends Migration
             Logging::info('-------------------------');
             foreach ($modules as $key => $module) {
                 $module_structure = $module['structure'];
+                Logging::info('It will check '.$module['key']);
                 if (isset($module_structure) && isset($module_structure['columns'])) {
                     foreach ($module_structure['columns'] as $column_key => $column) {
                         if (isset($column['components'])) {
                             foreach ($column['components'] as $component_key => $component) {
-                                Logging::info('It will check '.$module['key']);
                                 if (isset($component['type'])
                                     && ($component['type'] === 'button-element')
                                     && isset($component['button'])
