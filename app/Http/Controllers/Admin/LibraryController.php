172ac691b03b2019250c039f02a57fc4a2702690
Ximena Garcia <ximena.garcia@stensul.com>
2018-11-27 09:39:06 -0300

    [ADMIN-87] - Fix permission for controller

--- app/Http/Controllers/Admin/LibraryController.php
+++ app/Http/Controllers/Admin/LibraryController.php
@@ -34,6 +34,7 @@ class LibraryController extends Controller
     {
         $this->middleware('AdminAuthenticate');
         $this->middleware('acl.permission:access_admin_libraries');
+        $this->middleware('acl.permission:access_admin_studio_libraries');
     }
 
     /**
