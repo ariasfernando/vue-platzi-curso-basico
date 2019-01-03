9420cc4786c89b692286e6fb3679d0a996a1b3b1
Daniel Villalba <daniel@stensul.com>
2019-01-03 10:07:45 -0300

    Merge remote-tracking branch 'origin/SV2-882_research_minimum_coverage' into SV2-1200_add_the_coverage_metrics

--- resources/assets/vue/plugins/modules/text-color-by-background/campaignSettings.vue
+++ resources/assets/vue/plugins/modules/text-color-by-background/campaignSettings.vue
@@ -1,49 +1,47 @@
 
 
 <script>
-  import _ from 'lodash';
-  import contrast from 'contrast';
+import _ from 'lodash';
+import contrast from 'contrast';
 
-  export default {
-    props: ['name', 'plugin', 'columnId'],
-    computed: {
-      currentModule() {
-        return this.$store.getters["campaign/currentModule"];
-      },
-      module() {
-        return this.$store.getters["campaign/modules"][this.currentModule];
-      },
-      bgcolor() {
-        return this.module.structure.columns[this.columnId].container.attribute.bgcolor;
-      },
+export default {
+  props: ['name', 'plugin', 'columnId'],
+  computed: {
+    currentModule() {
+      return this.$store.getters['campaign/currentModule'];
     },
-    watch: {
-      bgcolor(bgcolor) {
-        const color = contrast(bgcolor) === 'light' ? this.plugin.config.darkText : this.plugin.config.lightText;
-
-        // Loop through columns and components
-        _.each(this.module.structure.columns[this.columnId].components, (comp, compId) => {
-          const subComponent = comp.type.split('-')[0];
-          // Set new text color
-          if (subComponent === 'text') {
-            this.$store.commit('campaign/saveComponentProperty', {
-              moduleId: this.currentModule,
-              columnId: this.columnId,
-              componentId: compId,
-              subComponent,
-              link: 'style',
-              property: 'color',
-              value: color,
-            })
-            
-            // reset tinymce to refresh changes on text
-            const editorId = ['editor', this.module.idInstance, this.columnId, compId].join('-');
-            setTimeout(() => {
-              document.getElementById(editorId).dispatchEvent(new Event("tiny-style-reset"));
-            }, 10);
-          }
-        });
-      }
+    module() {
+      return this.$store.getters['campaign/modules'][this.currentModule];
     },
-  }
+    bgcolor() {
+      return this.module.structure.columns[this.columnId].container.attribute.bgcolor;
+    },
+  },
+  watch: {
+    bgcolor(bgcolor) {
+      const color = contrast(bgcolor) === 'light' ? this.plugin.config.darkText : this.plugin.config.lightText;
+      // Loop through columns and components
+      _.each(this.module.structure.columns[this.columnId].components, (comp, compId) => {
+        const subComponent = comp.type.split('-')[0];
+        // Set new text color
+        if (subComponent === 'text') {
+          this.$store.commit('campaign/saveComponentProperty', {
+            moduleId: this.currentModule,
+            columnId: this.columnId,
+            componentId: compId,
+            subComponent,
+            link: 'style',
+            property: 'color',
+            value: color,
+          });
+          // reset tinymce to refresh changes on text
+          const editorId = ['editor', this.module.idInstance, this.columnId, compId].join('-');
+          setTimeout(() => {
+            document.getElementById(editorId).dispatchEvent(new Event('tiny-style-reset'));
+          }, 10);
+        }
+      });
+    },
+  },
+};
 </script>
