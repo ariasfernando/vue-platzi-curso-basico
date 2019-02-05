52d5576d9756852527cdc3caf2672a08c89d7aff
Facundo García Juliá <facundo.garcia@stensul.com>
2019-02-05 18:18:12 -0300

    [SV2-718] Update plugin dataDescription

--- resources/assets/vue/plugins/modules/mask-link/studioSettings.vue
+++ resources/assets/vue/plugins/modules/mask-link/studioSettings.vue
@@ -1,111 +1,82 @@
 <template>
-  <div v-show="this.component.plugins.destinationUrl.enabled">
-  <settings-container :label="plugin.title">
+  <div v-show="component.plugins.destinationUrl.enabled">
+    <SettingsContainer :label="plugin.title">
       <template slot="setting-right">
-          <toggle-button :value="enabled" @change="toggle"></toggle-button>
+        <ToggleButton :value="plugin.enabled" @change="toggle" />
       </template>
-    </settings-container>
-    <settings-container :label="'Default mask text'">
+    </SettingsContainer>
+    <SettingsContainer :label="'Default mask text'">
       <template slot="setting-bottom">
-        <el-input
+        <ElInput
           v-if="enabled"
           v-model="dataDescription"
-          placeholder="Enter default mask link"
-        ></el-input>
+          placeholder="Enter default mask link" />
       </template>
-  </settings-container>
+    </SettingsContainer>
   </div>
 </template>
 
 <script>
+import SettingsContainer from '../../../components/common/settings/containers/SettingsContainer.vue';
+import pluginMixin from '../mixins/pluginMixinAdmin';
 
-  import _ from 'lodash';
-  import SettingsContainer from "../../../components/common/settings/containers/SettingsContainer.vue";
-  import pluginMixin from '../mixins/pluginMixin';
-
-  export default {
-    props: {
-      name: {
-        type: String,
-      },
+export default {
+  components: { SettingsContainer },
+  mixins: [pluginMixin],
+  props: {
+    name: {
+      type: String,
     },
-    computed: {
-      currentComponent() {
-        return this.$store.getters["module/currentComponent"];
+  },
+  computed: {
+    dataDescription: {
+      get() {
+        return 'options' in this.plugin.config &&
+          this.plugin.config.options.data
+          ? this.plugin.config.options.data
+          : '';
       },
-      module() {
-        return this.$store.getters["module/module"];
-      },
-      plugin() {
-        const module = this.module,
-          columnId = this.currentComponent.columnId,
-          componentId = this.currentComponent.componentId;
+      set(value) {
+        const options = {
+          data: value,
+        };
 
-        const plugin =
-          module.structure.columns[columnId].components[componentId].plugins[
-            this.name
-          ];
-        this.enabled = plugin.enabled;
-        this.options = plugin.config.options;
-
-        return plugin;
-      },
-      dataDescription: {
-        get() {
-          return 'options' in this.plugin.config && this.plugin.config.options.data
-            ? this.plugin.config.options.data
-            :'';
-        },
-        set(value) {
-          const options = {
-            data: value
-          };
-
-          const payload = {
-            plugin: this.name,
-            columnId: this.currentComponent.columnId,
-            componentId: this.currentComponent.componentId,
-            config: {
-              options
-            }
-          };
-
-          // Save plugin data
-          this.$store.commit("module/savePlugin", payload);
-        }
-      }
-    },
-    mixins: [pluginMixin],
-    components: { SettingsContainer },
-    watch: {
-      component: {
-        handler: function() {
-          this.plugin.subComponent = this.component.type === 'button-element' ? 'button' : 'image';
-          if (!this.component.plugins.destinationUrl.enabled) {
-            this.toggle(false);
-          }
-        },
-        deep: true,
-      },
-    },
-    created() {
-    },
-    data() {
-      return {
-        enabled: false,
-        options: {}
-      }
-    },
-    methods: {
-      toggle(value) {
         const payload = {
           plugin: this.name,
           columnId: this.currentComponent.columnId,
           componentId: this.currentComponent.componentId,
-          enabled: value,
+          config: {
+            options,
+          },
         };
-        this.$store.commit('module/togglePlugin', payload);
+
+        // Save plugin data
+        this.$store.commit('module/savePlugin', payload);
       },
-    }
-  }
+    },
+  },
+  watch: {
+    component: {
+      handler() {
+        this.plugin.subComponent =
+          this.component.type === 'button-element' ? 'button' : 'image';
+        if (!this.component.plugins.destinationUrl.enabled) {
+          this.toggle(false);
+        }
+      },
+      deep: true,
+    },
+  },
+  methods: {
+    toggle(value) {
+      const payload = {
+        plugin: this.name,
+        columnId: this.currentComponent.columnId,
+        componentId: this.currentComponent.componentId,
+        enabled: value,
+      };
+      this.$store.commit('module/togglePlugin', payload);
+    },
+  },
+};
 </script>
