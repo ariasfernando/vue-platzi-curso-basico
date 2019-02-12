cb9392736d6c62ca501e428c94e603c78270cc37
Leandro Romero <leandro@Leandros-MacBook-Pro.local>
2019-02-12 15:21:47 -0300

    Add ids

--- resources/assets/vue/components/admin/Modules.vue
+++ resources/assets/vue/components/admin/Modules.vue
@@ -4,7 +4,7 @@
     <div class="row">
       <div class="col-xs-12">
         <h2 class="pull-left">Module List</h2>
-        <div class="btn btn-default btn-create pull-right">
+        <div class="btn btn-default btn-create pull-right" id="createNewModuleButton">
           <router-link to="/create"><i class="glyphicon glyphicon-plus-sign"></i> Create a new Module</router-link>
         </div>
       </div>
