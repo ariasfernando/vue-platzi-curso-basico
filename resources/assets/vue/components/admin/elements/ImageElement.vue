a3d216454f2ac85fbb024520286d38dae9b427ea
Ximena Garcia <ximena.garcia@stensul.com>
2019-02-13 14:40:28 -0300

    [SV2-1364] - Fix linting

--- resources/assets/vue/components/admin/elements/ImageElement.vue
+++ resources/assets/vue/components/admin/elements/ImageElement.vue
@@ -1,6 +1,9 @@
 <template>
-  <module-container :component="component" :is-active="isActive" @select-component="selectComponentHandler">
-      <table
+  <ModuleContainer
+    :component="component"
+    :is-active="isActive"
+    @select-component="selectComponentHandler">
+    <table
       :width="component.container.attribute.width || '100%'"
       :style="{width:widthStyle(component.container.attribute.width || '100%')}"
       :valign="component.container.attribute.valign || 'top'"
@@ -8,65 +11,65 @@
       border="0"
       cellpadding="0"
       cellspacing="0">
-        <tr>
-          <td 
-            width="100%"
-            :valign="component.image.attribute.valign || 'top'"
-            :align="component.image.attribute.align"
-            :bgcolor="component.image.attribute.bgcolor"
-            style="width:100%;"
+      <tr>
+        <td
+          width="100%"
+          :valign="component.image.attribute.valign || 'top'"
+          :align="component.image.attribute.align"
+          :bgcolor="component.image.attribute.bgcolor"
+          style="width:100%;"
           :style="elementBorderAndPadding(component.image)">
-            <a 
-              :href="component.image.attribute.href" 
-              :alt="component.image.attribute.alt"
-              :title="component.image.attribute.title"
-              :target="component.image.attribute.target || '_blank'"
+          <a
+            :href="component.image.attribute.href"
+            :alt="component.image.attribute.alt"
+            :title="component.image.attribute.title"
+            :target="component.image.attribute.target || '_blank'"
             @click.prevent>
-              <img
-                :class="{ 'st-hide-mobile' : component.image.attribute.placeholderMobile,
+            <img
+              :class="{ 'st-hide-mobile' : component.image.attribute.placeholderMobile,
                         'st-resize' : mobileStretch,
                         'st-mobile-width-constraint' : !mobileStretch }"
-                style="border: 0; display: block;"
-                border="0"
-                :width="imageWidth"
-                :style="{width: widthStyle(imageWidth), 'max-width': imageMaxWidth}"
-                :src="imageUrl(component.image.attribute.placeholder)"
-                :height="component.image.attribute.height === 'auto' ? undefined : component.image.attribute.height"
-                :alt="component.image.attribute.alt"
-                :title="component.image.attribute.title"
-              >
-              <template 
-                v-if="component.image.attribute.placeholderMobile">
-                <div class="show-img-mobile" style="display:none;width:0;overflow:hidden;max-height:0!important;">
-                  <img
-                    :src="imageUrl(component.image.attribute.placeholderMobile)"
-                    border="0"
-                    :class="{ 'st-resize' : mobileStretch,
-                              'st-mobile-width-constraint' : !mobileStretch }"
-                    style="display:block;border:none;max-width:100%;height:auto;"
-                    :width="imageWidth"
-                    :style="{width: widthStyle(imageWidth), 'max-width': imageMaxWidth}"
-                    :height="component.image.attribute.height === 'auto' ? undefined : component.image.attribute.height"
-                    :alt="component.image.attribute.alt"
-                    :title="component.image.attribute.title"
-                  />
-                </div>
-              </template>
-            </a>
-            <component-toolbar :component-id="componentId" :column-id="columnId"></component-toolbar>
-          </td>
-        </tr>
-      </table>
-    </module-container>
+              style="border: 0; display: block;"
+              border="0"
+              :width="imageWidth"
+              :style="{width: widthStyle(imageWidth), 'max-width': imageMaxWidth}"
+              :src="imageUrl(component.image.attribute.placeholder)"
+              :height="component.image.attribute.height === 'auto' ? undefined : component.image.attribute.height"
+              :alt="component.image.attribute.alt"
+              :title="component.image.attribute.title">
+            <template v-if="component.image.attribute.placeholderMobile">
+              <div
+                class="show-img-mobile"
+                style="display:none;width:0;overflow:hidden;max-height:0!important;">
+                <img
+                  :src="imageUrl(component.image.attribute.placeholderMobile)"
+                  border="0"
+                  :class="{ 'st-resize' : mobileStretch,
+                            'st-mobile-width-constraint' : !mobileStretch }"
+                  style="display:block;border:none;max-width:100%;height:auto;"
+                  :width="imageWidth"
+                  :style="{width: widthStyle(imageWidth), 'max-width': imageMaxWidth}"
+                  :height="component.image.attribute.height === 'auto' ? undefined : component.image.attribute.height"
+                  :alt="component.image.attribute.alt"
+                  :title="component.image.attribute.title">
+              </div>
+            </template>
+          </a>
+          <ComponentToolbar :component-id="componentId" :column-id="columnId" />
+        </td>
+      </tr>
+    </table>
+  </ModuleContainer>
   <!-- IMAGE ELEMENT ENDS -->
 </template>
 
 <script>
-import ComponentToolbar from "./ComponentToolbar.vue";
-import MobileStylesMixin from "../../common/mixins/MobileStylesMixin";
-import ElementMixin from "../../common/mixins/ElementMixin";
-import PlaceholderMixin from "../../common/mixins/PlaceholderMixin";
-import ModuleContainer from "../../common/containers/ModuleContainer.vue";
+import ComponentToolbar from './ComponentToolbar.vue';
+import MobileStylesMixin from '../../common/mixins/MobileStylesMixin';
+import ElementMixin from '../../common/mixins/ElementMixin';
+import PlaceholderMixin from '../../common/mixins/PlaceholderMixin';
+import ModuleContainer from '../../common/containers/ModuleContainer.vue';
+
 export default {
   name: 'ImageElement',
   components: {
@@ -74,36 +77,39 @@ export default {
     ModuleContainer,
   },
   mixins: [MobileStylesMixin, ElementMixin, PlaceholderMixin],
+  computed: {
+    mobileStretch() {
+      return this.component.image.styleOption.noMobileStretch !== true;
+    },
+  },
   methods: {
     imageUrl(imagePath) {
-      if (imagePath === "" || imagePath.includes("default/")) {
+      if (imagePath === '' || imagePath.includes('default/')) {
         const width =
-          this.component.image.attribute.width === "100%"
+          this.component.image.attribute.width === '100%'
             ? this.columnWidth(this.columnId)
             : this.component.image.attribute.width;
-        let ratio = 9 / 16;
+        let ratio = 0;
         switch (this.component.image.attribute.ratio) {
-          case "1:1":
+          case '1:1':
             ratio = 1;
             break;
-          case "4:3":
+          case '4:3':
             ratio = 3 / 4;
             break;
+          case '16:9':
+          default:
+            ratio = 9 / 16;
         }
 
         return this.createPlaceholder(
           width,
           this.component.image.attribute.height,
-          ratio
+          ratio,
         );
       }
       return this.$_app.config.imageUrl + imagePath;
-    }
-  },
-  computed: {
-    mobileStretch() {
-      return this.component.image.styleOption.noMobileStretch !== true;
-    }
+    },
   },
 };
 </script>
--- resources/assets/vue/components/campaign/elements/ImageElement.vue
+++ resources/assets/vue/components/campaign/elements/ImageElement.vue
@@ -1,5 +1,5 @@
 <template>
-  <module-container :component="component" @select-component="selectComponentHandler">
+  <ModuleContainer :component="component" @select-component="selectComponentHandler">
     <table
       :width="component.container.attribute.width || '100%'"
       :style="{width:widthStyle(component.container.attribute.width || '100%')}"
@@ -9,12 +9,13 @@
       cellpadding="0"
       cellspacing="0">
       <tr>
-        <td width="100%"
-            :valign="component.image.attribute.valign || 'top'"
-            :align="component.image.attribute.align"
-            :bgcolor="component.image.attribute.bgcolor"
-            style="width:100%;"
-            :style="elementBorderAndPadding(component.image)">
+        <td
+          width="100%"
+          :valign="component.image.attribute.valign || 'top'"
+          :align="component.image.attribute.align"
+          :bgcolor="component.image.attribute.bgcolor"
+          style="width:100%;"
+          :style="elementBorderAndPadding(component.image)">
           <a
             :href="component.image.attribute.href"
             :alt="component.image.attribute.alt"
@@ -35,9 +36,10 @@
               :style="{width: widthStyle(imageWidth), 'max-width': imageMaxWidth}"
               :alt="component.image.attribute.alt"
               :title="component.image.attribute.title">
-            <template
-              v-if="component.image.attribute.placeholderMobile">
-              <div class="show-img-mobile" style="display:none;width:0;overflow:hidden;max-height:0!important;">
+            <template v-if="component.image.attribute.placeholderMobile">
+              <div
+                class="show-img-mobile"
+                style="display:none;width:0;overflow:hidden;max-height:0!important;">
                 <img
                   :src="imageUrl(component.image.attribute.placeholderMobile)"
                   border="0"
@@ -56,55 +58,58 @@
         </td>
       </tr>
     </table>
-  </module-container>
-<!-- IMAGE ELEMENT ENDS -->
+  </ModuleContainer>
+  <!-- IMAGE ELEMENT ENDS -->
 </template>
 
 
 <script>
-  import MobileStylesMixin from '../../common/mixins/MobileStylesMixin.js';
-  import ModuleContainer from '../../common/containers/ModuleContainer';
-  import ElementMixin from '../../common/mixins/ElementMixin.js';
-  import PlaceholderMixin from '../../common/mixins/PlaceholderMixin';
+import MobileStylesMixin from '../../common/mixins/MobileStylesMixin';
+import ModuleContainer from '../../common/containers/ModuleContainer.vue';
+import ElementMixin from '../../common/mixins/ElementMixin';
+import PlaceholderMixin from '../../common/mixins/PlaceholderMixin';
 
-  export default {
-    name: 'ImageElement',
-    components: {
-      ModuleContainer,
-    },
-    mixins: [MobileStylesMixin, ElementMixin, PlaceholderMixin],
-    data() {
-      return {
-        imageUrl(imagePath) {
-        if (imagePath === "" || imagePath.includes("default/")) {
+export default {
+  name: 'ImageElement',
+  components: {
+    ModuleContainer,
+  },
+  mixins: [MobileStylesMixin, ElementMixin, PlaceholderMixin],
+  data() {
+    return {
+      imageUrl(imagePath) {
+        if (imagePath === '' || imagePath.includes('default/')) {
           const width =
-            this.component.image.attribute.width === "100%"
+            this.component.image.attribute.width === '100%'
               ? this.columnWidth(this.columnId)
               : this.component.image.attribute.width;
-          let ratio = 9 / 16;
+          let ratio = 0;
           switch (this.component.image.attribute.ratio) {
-            case "1:1":
+            case '1:1':
               ratio = 1;
               break;
-            case "4:3":
+            case '4:3':
               ratio = 3 / 4;
               break;
-            }
+            case '16:9':
+            default:
+              ratio = 9 / 16;
+          }
 
           return this.createPlaceholder(
             width,
             this.component.image.attribute.height,
-            ratio
+            ratio,
           );
-          }
-          return this.$_app.config.imageUrl + imagePath;
-      }
-      };
-    },
-    computed: {
-      mobileStretch() {
-        return this.component.image.styleOption.noMobileStretch !== true;
+        }
+        return this.$_app.config.imageUrl + imagePath;
       },
+    };
+  },
+  computed: {
+    mobileStretch() {
+      return this.component.image.styleOption.noMobileStretch !== true;
     },
-  };
+  },
+};
 </script>
