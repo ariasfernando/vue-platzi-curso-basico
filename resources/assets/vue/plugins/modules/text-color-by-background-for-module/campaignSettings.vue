993f90f9b621b9ae9d0dde4ca72d723f33a2792b
Facundo García Juliá <facundo.garcia@stensul.com>
2019-02-25 17:40:07 -0300

    [STD-522] Update text-color-by-background-for-module

--- resources/assets/vue/plugins/modules/text-color-by-background-for-module/campaignSettings.vue
+++ resources/assets/vue/plugins/modules/text-color-by-background-for-module/campaignSettings.vue
@@ -16,24 +16,26 @@ export default {
     bgcolor(bgcolor) {
       const value = contrast(bgcolor) === 'light' ? this.plugin.config.darkText : this.plugin.config.lightText;
       // Loop through columns and components
-      _.each(this.element.structure.columns, (column, columnId) => {
-        _.each(this.element.structure.columns[columnId].components, (comp, compId) => {
-          const subComponent = comp.type.split('-')[0];
-          // Set new text color
-          if (subComponent === 'text') {
-            this.saveElementProperty({
-              elementId: comp.id,
-              subComponent,
-              link: 'style',
-              property: 'color',
-              value,
-            });
-            // reset tinymce to refresh changes on text
-            const editorId = this.getTinyId(this.moduleIdInstance, comp.id);
-            setTimeout(() => {
-              document.getElementById(editorId).dispatchEvent(new Event('tiny-style-reset'));
-            }, 10);
-          }
+      _.each(this.element.structure.rows, (row) => {
+        _.each(rows.columns, (column) => {
+          _.each(column.components, (component) => {
+            const subComponent = component.type.split('-')[0];
+            // Set new text color
+            if (subComponent === 'text') {
+              this.saveElementProperty({
+                elementId: component.id,
+                subComponent,
+                link: 'style',
+                property: 'color',
+                value,
+              });
+              // reset tinymce to refresh changes on text
+              const editorId = this.getTinyId(this.moduleIdInstance, component.id);
+              setTimeout(() => {
+                document.getElementById(editorId).dispatchEvent(new Event('tiny-style-reset'));
+              }, 10);
+            }
+          });
         });
       });
     },
