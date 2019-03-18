b9d1c4ec254bd3cfa8d7fa6b9bbd88031da98180
Sebastian Pons <sebastian@fanbridge.com>
2019-03-18 10:56:15 -0300

    [CYL-19] enable marketo

---
+++ stensul/customer/config/expose.php
@@ -0,0 +1,23 @@
+<?php
+return [
+    'admin' => '*',
+    'global_settings' => '*',
+    'campaign' => '*',
+    'tracking' => '*',
+    'locale' => '*',
+    'menu' => '*',
+    'modals' => '*',
+    'modules' => '*',
+    'proof' => '*',
+    'view' => '*',
+    'api' => [
+        'marketo' => [
+            'title', 'class'
+        ],
+    ],
+    'esp' => [
+        'marketo' => [
+            'title', 'class'
+        ],
+    ],
+];
