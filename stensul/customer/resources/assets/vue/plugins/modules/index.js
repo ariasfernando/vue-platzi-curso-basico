b877836584d49f1a1e167b405ef46be07bff4531
Elias Torres <elias.torres@stensul.com>
2019-04-09 00:46:26 -0300

    [CYL-34] added new plugin

--- stensul/customer/resources/assets/vue/plugins/modules/index.js
+++ stensul/customer/resources/assets/vue/plugins/modules/index.js
@@ -1,7 +1,9 @@
 import customTextColorByBgcolor from './custom-text-color-by-bgcolor';
+import customToggleIncrement from './custom-toggle-increment';
 import moduleRssFeed from './module-rss-feed';
 
 module.exports = {
   customTextColorByBgcolor,
+  customToggleIncrement,
   moduleRssFeed,
 };
