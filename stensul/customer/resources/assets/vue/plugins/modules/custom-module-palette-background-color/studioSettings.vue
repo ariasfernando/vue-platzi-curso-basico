4aabd38f1a4499c5a2ea19c6fa8abb75b90bdac5
Santiago Pernigotti <santiago.pernigotti@stensul.com>
2019-04-11 17:41:40 -0300

    [CYL-63] add custom module palette background color

---
+++ stensul/customer/resources/assets/vue/plugins/modules/custom-module-palette-background-color/studioSettings.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <SettingsContainer
+      :label="plugin.title"
+      :arrow="arrowState"
+      :label-expanded="true"
+      @toggleArrow="setSlideToggles">
+      <template slot="setting-right">
+        <StuiToggleButton :value="plugin.enabled" @change="toggle" />
+      </template>
+    </SettingsContainer>
+    <b-collapse :id="pluginKey" :visible="arrowState">
+      <SettingsContainer 
+        label="Use Palette from Library"
+        :label-expanded="true">
+        <template slot="setting-right">
+          <StuiToggleButton :value="plugin.config.usePaletteFromLibrary" @change="(value)=>updatePluginConfig({value, path:'usePaletteFromLibrary'})" />
+        </template>
+      </SettingsContainer>
+      <SettingsContainer v-if="plugin.config.usePaletteFromLibrary" label="Palette name">
+        <template slot="setting-right">
+          <StuiInputText
+            v-model="paletteFromLibrary"
+            placeholder="name" />
+        </template>
+      </SettingsContainer>
+      <SettingsContainer v-else label="Custom Palette">
+        <template slot="setting-right">
+          <StuiInputText
+            v-model="customPalette"
+            placeholder="000000,474646,79A8C9,CD202C" />
+        </template>
+      </SettingsContainer>
+    </b-collapse>
+  </div>
+</template>
+<script>
+import SettingsContainer from 'stensul/components/common/settings/containers/SettingsContainer.vue';
+import pluginMixinAdmin from 'stensul/plugins/modules/mixins/pluginMixinAdmin';
+
+export default {
+  components: { SettingsContainer },
+  mixins: [pluginMixinAdmin],
+  computed: {
+    paletteFromLibrary: {
+      get() {
+        return this.plugin.config.paletteName;
+      },
+      set(value) {
+        this.updatePluginConfig({value, path: 'paletteName'});
+      },
+    },
+    customPalette: {
+      get() {
+        return this.plugin.config.paletteMap.join(',');
+      },
+      set(value) {
+        this.updatePluginConfig({value: value.split(','), path: 'paletteMap'});
+      },
+    },
+  },
+
+};
+</script>
--- stensul/customer/resources/assets/vue/plugins/modules/index.js
+++ stensul/customer/resources/assets/vue/plugins/modules/index.js
@@ -1,9 +1,11 @@
 import customTextColorByBgcolor from './custom-text-color-by-bgcolor';
 import customToggleIncrement from './custom-toggle-increment';
+import customModulePaletteBackgroundColor from './custom-module-palette-background-color';
 import moduleRssFeed from './module-rss-feed';
 
 module.exports = {
   customTextColorByBgcolor,
   customToggleIncrement,
+  customModulePaletteBackgroundColor,
   moduleRssFeed,
 };
