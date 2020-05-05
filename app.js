5d39d62166af0cce5c4fa7bb63ef2ddc1d7f4f4f
Fernando Arias <fernando.arias.com>
2020-05-05 13:49:34 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      name: 'Bitcoin',
+      img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+      changePercent: -1,
+      prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200],
+      pricesWithDays: [
+        { day: 'Lunes', value: 8400 },
+        { day: 'Martes', value: 7900 },
+        { day: 'Miércoles', value: 8200 },
+        { day: 'Jueves', value: 9000 },
+        { day: 'Viernes', value: 9400 },
+        { day: 'Sábado', value: 10000 },
+        { day: 'Domingo', value: 10200 },
+      ]
+    }
+  }
+});
