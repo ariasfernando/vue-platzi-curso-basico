c9d2867989281584f3bcb99db521c4130c436f25
Fernando Arias <fernando.arias.com>
2020-05-14 11:49:25 -0300

    Delete Node Modules

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ new Vue({
     return {
       name: 'Bitcoin',
       img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-      changePercent: -1,
+      changePercent: 10,
+      
       prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200],
+
+      price: 8200,
+
       pricesWithDays: [
         { day: 'Lunes', value: 8400 },
         { day: 'Martes', value: 7900 },
@@ -14,7 +18,22 @@ new Vue({
         { day: 'Viernes', value: 9400 },
         { day: 'Sábado', value: 10000 },
         { day: 'Domingo', value: 10200 },
-      ]
+      ],
+      showPrices: false
+    }
+  },
+  methods: {
+    toggleShowPrices() {
+      this.showPrices = !this.showPrices;
     }
   }
 });
+
+/**
+ * * Necesidad de conjugar la vertical de conceptos de programación con tecnologías para el plan de capacitación.
+ * * Mejorar la documentación existente sobre el producto.
+ * * Aplicar accesibilidad como conocimiento.
+ * * Utilizar chinwenwencha como equipo de prueba.
+ * * Necesidad de tener side-projects específicos para aplicar los conocimientos.
+ * * Reforzar la parte teórica por detrás de las tecnologías en uso.
+ */
